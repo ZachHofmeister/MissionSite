0ef64486b2c8ad3523bce4cea1a8be7fa1e71197
ZachHofmeister <zachhof@me.com>
2023-02-12 22:13:59 -0700

    changed config.php

--- config.php
+++ config.php
@@ -1,8 +1,23 @@
 <?php 
+	// Toggle this to change the setting
+	// define('DEBUG', true);
+	// // You want all errors to be triggered
+	// error_reporting(E_ALL);
+	// ini_set('display_errors', DEBUG ? 'On' : 'Off');
+
 	session_start();
 
 	// connect to database
-	$conn = mysqli_connect('localhost', 'zach', 'zach', 'testdb');
+
+	$json = file_get_contents(".creds.json");
+	$creds = json_decode($json, true);
+
+	$host = $settings['host'] ?? null;
+	$user = $settings['username'] ?? null;
+	$pass = $settings ['password'] ?? null;
+	$db = $settings['schema'] ?? null;
+
+	$conn = mysqli_connect($host, $user, $pass, $db);
 	if (!$conn) {
 		die("Error connecting to database: " . mysqli_connect_error());
 	}
