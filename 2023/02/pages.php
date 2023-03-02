0b784bbdce47c783c0bf55afe13b9a19f0acd10f
ZachHofmeister <zachhof@me.com>
2023-03-02 14:18:29 -0700

    Finished writing Feb newsletter, made changes to newsletter.css

--- 2023/02/pages.php
+++ 2023/02/pages.php
@@ -1,7 +1,7 @@
 <!-- PAGE ONE -->
 <div class="page">
 	<!-- MAIN IMAGE / HEADER -->
-	<div class="page-section _40">
+	<div class="page-section _40"> 
 		<div class="page-item">
 			<img class="h100" src="2023/02/images/vp-group.jpeg" alt="" style="object-position: center 50%;">
 			<div class="title">
@@ -15,69 +15,71 @@
 	<div class="page-section _60">
 		<!-- TEXT -->
 		<div class="page-item triple text-justify">
-			<h2 class="italic">Hello Friends!</h2>
-			<h6 class="italic text-boxed">THANK YOU</h6>
-			<ul>
-				<li>one</li>
-				<li>two</li>
-				<li>three</li>
-			</ul>
+			<h2 class="italic">Dear Friends,</h2>
+			<p>
+				I hope that you are doing well! I am excited to tell you about some of the highlights from February here at the academy!
+			</p>
+			<h4>Archbishop Broglio's Visit</h4>
+			<p>
+				Each year, the Archbishop for the Military Services makes it a priority to visit us for the baptism and confirmation of cadets entering the Church! He met lots of cadets, celebrated mass for us, and spoke at our SPIRE meeting about his recent visit to Ukraine.
+			</p>
+			<h4>Airsoft Outreach</h4>
+			<p>
+				Getting to play with the Airsoft club was an awesome answer to prayer last semester, and this semester I've been able to join for many more game days! I've been inviting cadets I'm friends with to come play also and getting to know the others who come more and more. Please pray for the members of the club that God will work in their hearts and lives and provide opportunities to share the Gospel!
+			</p>
 		</div>
 		<!-- PHOTO STACK -->
 		<div class="page-item double photo-stack">
 			<div class="photo-item">
-				<img class="h70" src="/2023/02/images/nou-new.jpeg" alt="">
-				<p class="caption">Archbishop Broglio, Archbishop for the Military Services, visited us to baptize cadets like Nou, a convert from Buddhism! </p>
+				<img class="h80" src="/2023/02/images/nou-new.jpeg" alt="">
+				<p class="caption">Archbishop Broglio baptizing Nou, a convert from Buddhism! </p>
 			</div>
 			<div class="photo-item">
-				<img class="h80" src="/2023/02/images/pete-range.jpeg" alt="" style="object-position: 30% center">
-				<p class="caption">Pete and I had fun at the shooting range before our first growing discipleship meeting!</p>
+				<img class="h80" src="/2023/02/images/airsoft-josh.jpeg" alt="">
+				<p class="caption">Our group after a day in the airsoft field; we had a lot of Catholic cadets come this day as well as my teammate Josh!</p>
 			</div>
 		</div>
 	</div>
 </div>
 
 <!-- PAGE TWO -->
-
 <div class="page">
 	<!-- QUOTE -->
 	<div class="page-section _10">
 		<div class="page-item text-center center-center">
-			<h4 class="italic">QUOTE</h4>
+			<h3 class="italic">"And they devoted themselves to the apostles’ teaching and fellowship, to the breaking of bread and the prayers." - Acts 2:42</h3>
 		</div>
 	</div>
 
 	<!-- PARAGRAPH / IMAGES -->
-	<div class="page-section _30">
+	<div class="page-section _35">
 		<!-- PHOTO STACK -->
-		<div class="page-item double photo-stack">
+		<div class="page-item photo-stack">
 			<div class="photo-item">
-				<img class="h80" src="/2023/02/images/airsoft-josh.jpeg" alt="">
-				<p class="caption">We had a lot of cadets from the Catholic community as well as my teammate Josh come to this airsoft game!</p>
+				<img class="h80" src="/2023/02/images/pete-range.jpeg" alt="" style="object-position: 30% center">
+				<p class="caption">Pete and I had fun at the shooting range before our first growing discipleship meeting!</p>
 			</div>
 		</div>
 		<!-- TEXT -->
-		<div class="page-item triple text-justify">
+		<div class="page-item double text-justify">
+			<h4 class="text-right">Discipleship News</h4>
 			<p>
-				P
+				I have been having lots of good conversations in my growing discipleship meetings this month with Jared and Joe, who I mentioned last update, and I also recently started meeting with Pete! We have been discussing the four practices of the disciples in Acts 2:42 and how we can better incorporate them into our lives.
 			</p>
+			<h4 class="text-right">Other Highlights</h4>
+			<ul class="normal">
+				<li>Watching 200+ cadets receive ashes on Ash Wednesday</li>
+				<li>Singing karaoke with Zion</li>
+				<li>Taking a big group to Denver for Vigil Praise (The top photo is us stopping for dinner)</li>
+			</ul>
 		</div>
 	</div>
 
-	<!-- CLOSING -->
-	<!-- <div class="page-section _10">
-		<div class="page-item text-center">
-			<p>
-				P
-			</p>
-		</div>
-	</div> -->
-
 	<!-- PHOTOS -->
-	<div class="page-section _30">
+	<div class="page-section _25">
 		<div class="page-item">
 			<img class="h80" src="/2023/02/images/ablaze-prodigal.jpeg" alt=""/>
-			<p class="caption">This month, our Ablaze night (where we have adoration, praise, and confession) was focused on the parable of the prodigal son and God our Father.</p>
+			<p class="caption">The Ablaze event this month was focused on God our Father and the return of the prodigal son!</p>
 		</div>
 		<div class="page-item">
 			<img class="h80" src="/2023/02/images/bishop-neophytes.jpeg" alt="" style="object-position: center 60%;"/>
@@ -89,8 +91,11 @@
 	<div class="page-section _20">
 		<div class="page-item double text-right">
 			<h6 class="italic text-boxed">THANK YOU</h6>
-			<p class="small">
-				Thank you!
+			<p class="">
+				Thank you so much for continuing to be a part of this mission! I am praying for you always!
+			</p>
+			<p>
+				Yours in Christ, Zach
 			</p>
 		</div>
 		<div class="page-item">
@@ -98,10 +103,10 @@
 		</div>
 		<div class="page-item double">
 			<h6 class="italic text-boxed">PLEASE PRAY</h6>
-			<ul>
-				<li>For the cadets I am walking with in discipleship</li>
-				<li>For our outreach to new cadets on campus</li>
-				<li>That I might surrender more fully to the Lord and trust Him more</li>
+			<ul class="normal">
+				<li>For everyone who came for Ash Wednesday</li>
+				<li>For the airsoft club cadets</li>
+				<li>For the cadets in bible studies and discipleship</li>
 			</ul>
 		</div>
 	</div>
