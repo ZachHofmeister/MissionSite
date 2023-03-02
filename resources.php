03c837e0e01faa539c30552f6bdb4804b896735e
ZachHofmeister <zachhof@me.com>
2023-03-02 16:20:39 -0700

    finishing touches feb newsletter

--- resources.php
+++ resources.php
@@ -16,15 +16,23 @@
 		<div id="main">
 			<div class="section padded">
 				<h1 class="text-center">Resources</h1>
-				<p class="text-justify">I love bringing you into the mission we are on together through my monthly newsletters, but there is even more I would like to share with you! On this page, I plan to list some of the resources that have helped me grow in my relationship with God and that I share with students. I hope that the Holy Spirit may use them to bless you also!</p>
+				<p>
+					I love bringing you into the mission we are on together through my monthly newsletters, but there is even more I would like to share with you! On this page, I plan to list some of the resources that have helped me grow in my relationship with God and that I share with students. I hope that the Holy Spirit may use them to bless you also!
+				</p>
 			</div>
 
 			<!-- <div class="section bordered">
 				<h2>Bible Studies</h2>
-				<p>An important part of our week is leading and inviting students to weekly Bible studies. In Bible study, we help students learn more about who God is and how to follow Him while building virtuous friendships.</p>
-				<p>Typically, we lead bible studies written by FOCUS. While we could and sometimes do use outside sources or come up with our own material, we do this so that as our students begin to lead their own bible studies they have a clear roadmap of how to lead others.</p>
-				<p>You can find the bible studies we use at <a href="https://focusequip.org/bible-study-resources/" target="_blank" class="hover-underline">focusequip.org</a>.</p>
-				<p>This semester, the studies I am leading are:</p>
+				<p>
+					An important part of our week is leading and inviting students to weekly Bible studies. In Bible study, we help students learn more about who God is and how to follow Him while building virtuous friendships.
+				</p>
+				<p>
+					Typically, we lead bible studies written by FOCUS so that as our students begin to lead their own bible studies they have a clear roadmap of how to lead others.
+				</p>
+				<p>
+					You can find the bible studies we use at <a href="https://focusequip.org/bible-study-resources/" target="_blank" class="hover-underline">focusequip.org</a>.
+				</p>
+				<p>This semester, I am leading:</p>
 				<ul>
 					<li>The Story of Salvation (the 'old' version)</li>
 					<li>The Big 3 (Men)</li>
