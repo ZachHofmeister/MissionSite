329e5041610235e18e445041fff2c93b2b34c933
ZachHofmeister <zachhof@me.com>
2023-03-15 11:28:38 -0600

    started working on march newsletter

---
+++ 2023/03/head.php
@@ -0,0 +1,2 @@
+<title>March 2023 Update</title>
+<link rel="stylesheet" type="text/css" href="/2023/03/colors.css">
---
+++ 2023/03/pages.php
@@ -0,0 +1,125 @@
+<!-- Yours, O Lord, are the greatness, the power, the glory, the victory, and the majesty; for all that is in the heavens and on the earth is yours; yours is the kingdom, O Lord, and you are exalted as head above all. - 1 Chronicles 29:11 -->
+
+<!-- PAGE ONE -->
+<div class="page">
+	<!-- MAIN IMAGE / HEADER -->
+	<div class="page-section _40"> 
+		<div class="page-item">
+			<img class="h100" src="2023/03/images/ski-party.jpeg" alt="" style="object-position: center 40%;">
+			<div class="title">
+				<h1>Zach's Mission Update</h1>
+				<h4 class="italic">US Air Force Academy <span class="no-style">|</span> March 2023</h4>
+			</div>
+		</div>
+	</div>
+	
+	<!-- PARAGRAPH / IMAGES -->
+	<div class="page-section _60">
+		<!-- TEXT -->
+		<div class="page-item triple text-justify">
+			<h2 class="italic">Dear Friends,</h2>
+			<h4>Section</h4>
+			<p>
+				Paragraph
+			</p>
+		</div>
+		<!-- PHOTO STACK -->
+		<div class="page-item double photo-stack">
+			<div class="photo-item">
+				<img class="h80" src="/2023/03/images/nou-new.jpeg" alt="">
+				<p class="caption">CAPTION</p>
+			</div>
+			<div class="photo-item">
+				<img class="h80" src="/2023/03/images/airsoft-josh.jpeg" alt="">
+				<p class="caption">CAPTION</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE TWO -->
+<div class="page">
+	<!-- QUOTE -->
+	<div class="page-section _10">
+		<div class="page-item text-center center-center">
+			<h3 class="italic">"And they devoted themselves to the apostles’ teaching and fellowship, to the breaking of bread and the prayers." - Acts 2:42</h3>
+		</div>
+	</div>
+
+	<!-- PARAGRAPH / IMAGES -->
+	<div class="page-section _35">
+		<!-- PHOTO STACK -->
+		<div class="page-item photo-stack">
+			<div class="photo-item">
+				<img class="h80" src="/2023/03/images/pete-range.jpeg" alt="" style="object-position: 30% center">
+				<p class="caption">CAPTION</p>
+			</div>
+		</div>
+		<!-- TEXT -->
+		<div class="page-item double text-justify">
+			<h4 class="text-right">Section</h4>
+			<p>
+				Paragraph
+			</p>
+			<h4 class="text-right">Other Highlights</h4>
+			<ul class="normal">
+				<li>List</li>
+			</ul>
+		</div>
+	</div>
+
+	<!-- PHOTOS -->
+	<div class="page-section _25">
+		<div class="page-item">
+			<img class="h80" src="/2023/03/images/ablaze-prodigal.jpeg" alt=""/>
+			<p class="caption">CAPTION</p>
+		</div>
+		<div class="page-item">
+			<img class="h80" src="/2023/03/images/bishop-neophytes.jpeg" alt="" style="object-position: center 60%;"/>
+			<p class="caption">CAPTION</p>
+		</div>
+	</div>
+
+	<!-- THANK YOU / PHOTO / PRAYERS -->
+	<div class="page-section _20">
+		<div class="page-item double text-right">
+			<h6 class="italic text-boxed">THANK YOU</h6>
+			<p class="">
+				Thank you!
+			</p>
+			<p>
+				Yours in Christ, Zach
+			</p>
+		</div>
+		<div class="page-item">
+			<img class="h100" src="/2023/03/images/airsoft-pjnc.jpeg" alt="" style="object-position: 0 0">
+		</div>
+		<div class="page-item double">
+			<h6 class="italic text-boxed">PLEASE PRAY</h6>
+			<ul class="normal">
+				<li>For...</li>
+			</ul>
+		</div>
+	</div>
+	
+	<!-- FOOTER -->
+	<div class="page-section _10">
+		<div class="page-item double text-left vert-center">
+			<h5>
+				14219 Woodrock Path<br>
+				Colorado Springs, CO 80921
+			</h5>
+		</div>
+		<div class="page-item vert-center">
+			<a href="https://www.focus.org" target="_blank">
+				<img class="contain no-lightbox" src="/2023/03/images/focus-logo.png" alt="FOCUS logo">
+			</a>
+		</div>
+		<div class="page-item double text-right vert-center">
+			<h5>
+				<a href='tel:+16196558686' class='no-decor'>(619) 655-8686</a><br>
+				<a href='mailto:edward.hofmeister@focus.org' class='no-decor'>edward.hofmeister@focus.org</a>
+			</h5>
+		</div>
+	</div>
+</div>
