1b74091e2bbe14807cc733fca7e319eabac972b6
ZachHofmeister <zachhof@me.com>
2023-04-06 05:11:34 +0100

    small edit

--- 2023/03/pages.php
+++ 2023/03/pages.php
@@ -78,7 +78,7 @@
 			<ul class="normal text-left">
 				<li>Meeting new guys at brunch after mass</li>
 				<li>Picking Pete up from the airport</li>
-				<li>Seeing my family and friends over break</li>
+				<li>Visiting family and friends in California over break</li>
 			</ul>
 		</div>
 		<div class="page-item">
