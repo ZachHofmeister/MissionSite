b02422facfd8e4c6dabe58d0a8414235ccb7b250
ZachHofmeister <zachhof@me.com>
2023-04-06 15:35:58 -0600

    Started working on April newsletter

---
+++ 2023/04/pages.php
@@ -0,0 +1,139 @@
+<!-- Yours, O Lord, are the greatness, the power, the glory, the victory, and the majesty; for all that is in the heavens and on the earth is yours; yours is the kingdom, O Lord, and you are exalted as head above all. - 1 Chronicles 29:11 -->
+
+<!-- PAGE ONE -->
+<div class="page">
+	<!-- MAIN IMAGE / HEADER -->
+	<div class="page-section _40"> 
+		<div class="page-item">
+			<img class="h100" src="2023/03/images/ski-party.jpeg" alt="" style="object-position: center 40%;">
+			<div class="title">
+				<h1>Zach's Mission Update</h1>
+				<h4 class="italic">US Air Force Academy<span class="no-style"> | </span>March 2023</h4>
+			</div>
+		</div>
+	</div>
+	
+	<!-- PARAGRAPH / IMAGES -->
+	<div class="page-section _60">
+		<!-- PHOTO STACK -->
+		<div class="page-item double photo-stack">
+			<div class="photo-item">
+				<img class="h80" src="/2023/03/images/fish-fry.jpeg" alt="">
+				<p class="caption">Pete and I went to a parish fish fry and prayed Stations of the Cross together one Friday!</p>
+			</div>
+			<div class="photo-item">
+				<img class="h80" src="/2023/03/images/sunday-catan.jpeg" alt="">
+				<p class="caption">After mass on Sundays this month I've been playing Settlers of Catan with some of my bible study men!</p>
+			</div>
+		</div>
+		<!-- TEXT -->
+		<div class="page-item triple text-justify">
+			<h2 class="italic">Dear Friends,</h2>
+			<p>
+				Happy spring! I'm so thankful that it looks like we'll be getting warmer weather for Easter this weekend. I am praying that you will have a great Holy Week, and hope you enjoy this little update from the Academy!
+			</p>
+			<h4 class="text-right">Skiing!</h4>
+			<p>
+				Near the beginning of March we took about 10 cadets up to Keystone for a Falcon Catholic ski day! The trip was a great opportunity to spend time with guys I don't see as much, meet new ones, and have good conversations on the road.
+			</p>
+			<h4 class="text-right">Discipleship Updates</h4>
+			<p>
+				Growing Discipleship has been going well with the three guys I am walking with, and this month I will be inviting them to start learning to lead their own bible studies for next semester! I am also inviting some of the seniors to walk with me in learning how they can share their faith and build community after they graduate. Please pray for them!
+			</p>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE TWO -->
+<div class="page">
+	<!-- QUOTE -->
+	<div class="page-section _10">
+		<div class="page-item text-center center-center">
+			<h4 class="italic">"It is too little, he says, for you to be my servant, to raise up the tribes of Jacob, and restore the survivors of Israel; I will make you a light to the nations, that my salvation may reach to the ends of the earth." - Isaiah 49:6</h3>
+		</div>
+	</div>
+
+	<!-- PARAGRAPH / IMAGES -->
+	<div class="page-section _35">
+		<!-- TEXT -->
+		<div class="page-item double text-justify">
+			<h4 class="">Lenten Journey</h4>
+			<p>
+				Since January I've been walking with a small group of men in <a href="https://exodus90.com/" target="_blank">Exodus 90</a>, a 90 day spiritual exercise concluding at easter that emphasizes prayer, asceticism, and fellowship. It has been difficult and far from perfect, but has been slowly helping us to realize our attachments and offer them to our Lord. One of the cadets has especially impressed me with his dedication to praying a daily holy hour, even though he could easily justify praying less. In the Easter celebration it can be easy to abandon what we've been learning in Lent, so please pray for us! I hope you have had a fruitful Lent and will have a joyous Easter!
+			</p>
+		</div>
+		<!-- PHOTO STACK -->
+		<div class="page-item photo-stack">
+			<div class="photo-item">
+				<img class="h70" src="/2023/03/images/blood-pressure.jpeg" alt="" style="object-position: 30% center">
+				<p class="caption">Sometimes it's good to get your blood pressure taken before bible study (I was helping Joe with his Emergency Medical Response club project)</p>
+			</div>
+		</div>
+	</div>
+
+	<!-- PHOTOS -->
+	<div class="page-section _25">
+		<div class="page-item">
+			<h4 class="">Other Highlights</h4>
+			<ul class="normal text-left">
+				<li>Meeting new guys at brunch after mass</li>
+				<li>Picking Pete up from the airport</li>
+				<li>Seeing my family and friends over break</li>
+			</ul>
+		</div>
+		<div class="page-item">
+			<img class="h70" src="/2023/03/images/kevin-lauren.jpeg" alt=""/>
+			<p class="caption">My friends Kevin and Lauren visited me from California at the beginning of spring break!</p>
+		</div>
+		<div class="page-item">
+			<img class="h70" src="/2023/03/images/trap-skeet.jpeg" alt="" style="object-position: center 60%;"/>
+			<p class="caption">Kevin, Lauren, and I went trap shooting with Joe, who is on the team at USAFA!</p>
+		</div>
+		
+	</div>
+
+	<!-- THANK YOU / PHOTO / PRAYERS -->
+	<div class="page-section _20">
+		<div class="page-item double text-right">
+			<h6 class="italic text-boxed">THANK YOU</h6>
+			<p class="">
+				It is so good to be with you on this journey. Know of my continued prayers for you!
+			</p>
+			<p>
+				Yours in Christ, Zach
+			</p>
+		</div>
+		<div class="page-item">
+			<img class="h90" src="/2023/03/images/ski-selfie.jpeg" alt="" style="object-position: 0 0">
+		</div>
+		<div class="page-item double">
+			<h6 class="italic text-boxed">PLEASE PRAY</h6>
+			<ul class="normal">
+				<li>For the men I am inviting into discipleship</li>
+				<li>For cadets away from the Church to come to Easter Mass</li>
+				<li>For greater love of Jesus' sacrifice</li>
+			</ul>
+		</div>
+	</div>
+	
+	<!-- FOOTER -->
+	<div class="page-section _10">
+		<div class="page-item double text-left vert-center">
+			<h5>
+				14219 Woodrock Path<br>
+				Colorado Springs, CO 80921
+			</h5>
+		</div>
+		<div class="page-item vert-center">
+			<a href="https://www.focus.org" target="_blank">
+				<img class="contain no-lightbox" src="/2023/03/images/focus-logo.png" alt="FOCUS logo">
+			</a>
+		</div>
+		<div class="page-item double text-right vert-center">
+			<h5>
+				<a href="tel:+16196558686" class="no-decor">(619) 655-8686</a><br>
+				<a href="mailto:edward.hofmeister@focus.org" class="no-decor">edward.hofmeister@focus.org</a>
+			</h5>
+		</div>
+	</div>
+</div>
