3b13c7ee21b53665aa69728d5488b936dbd2d7de
ZachHofmeister <zachhof@me.com>
2023-09-01 16:59:32 -0600

    working on new layout

---
+++ 2023/08/pages.php
@@ -0,0 +1,102 @@
+<!-- Yours, O Lord, are the greatness, the power, the glory, the victory, and the majesty; for all that is in the heavens and on the earth is yours; yours is the kingdom, O Lord, and you are exalted as head above all. - 1 Chronicles 29:11 -->
+
+<!-- PAGE ONE -->
+<div class="page">
+	<!-- NEW HEADER -->
+	<div class="page-section _10">
+		<div class="page-item vert-center">
+			<img class="contain no-lightbox" src="/2023/08/images/focus-logo.png" alt="FOCUS logo">
+		</div>
+		<div class="page-item vert-center text-center quint">
+			<h1 class="cardo">The Mission at Mesa</h1>
+			<h4 class="italic">Zach Hofmeister<span class="no-style"> | </span>Colorado Mesa University<span class="no-style"> | </span>August 2023</h4>
+		</div>
+		<div class="page-item vert-center">
+			<img class="contain no-lightbox" src="/2023/08/images/cmu-logo.png" alt="CMU logo">
+		</div>
+	</div>
+
+	<!-- MAIN IMAGE -->
+	<div class="page-section _40">
+		<div class="page-item">
+			<img class="h100" src="/2023/08/images/faith-fair.jpeg" alt="" style="object-position: 50% 20%;">
+		</div>
+	</div>
+
+	<!-- PARAGRAPH -->
+	<div class="page-section _25">
+		<div class="page-item">
+			<img class="h100" src="/2023/08/images/squad-walking.jpeg" alt="">
+		</div>
+		<div class="page-item">
+			<div class="box">
+				<h2 class="italic">Dear Friends,</h2>
+				<p class="large">Hello!</p>
+			</div>
+		</div>
+	</div>
+
+	<!-- PARAGRAPH -->
+	<div class="page-section _25">
+		<div class="page-item">
+			
+		</div>
+		<div class="page-item">
+			<img class="h100" src="/2023/08/images/club-fair.jpeg" alt="">
+		</div>
+	</div>
+</div>
+
+<!-- PAGE TWO -->
+<div class="page">
+	<!-- PARAGRAPH / IMAGES -->
+	<div class="page-section _30">
+		
+	</div>
+
+	<!-- PARAGRAPH / IMAGES -->
+	<div class="page-section _30">
+		
+	</div>
+
+	<!-- THANK YOU / PHOTO / PRAYERS -->
+	<div class="page-section _20">
+		<div class="page-item double text-right">
+			<h6 class="italic text-boxed">THANK YOU</h6>
+			<p class="">
+				Thank you!
+			</p>
+			<p>In Christ Jesus, Zach</p>
+		</div>
+		<div class="page-item">
+			<img class="h100" src="/2023/08/images/training-self.jpeg" alt="" style="object-position: 30% 50%;" data-caption="">
+		</div>
+		<div class="page-item double">
+			<h6 class="italic text-boxed">PLEASE PRAY</h6>
+			<ul class="normal">
+				<li>For ...</li>
+			</ul>
+		</div>
+	</div>
+	
+	<!-- FOOTER -->
+	<div class="page-section _10">
+		<div class="page-item double text-left vert-center">
+			<h5>
+				***REMOVED***<br>
+				Grand Junction, CO 81501
+			</h5>
+		</div>
+		<div class="page-item vert-center">
+			<a href="https://www.focus.org" target="_blank">
+				<img class="contain no-lightbox" src="/2023/08/images/focus-logo.png" alt="FOCUS logo">
+			</a>
+		</div>
+		<div class="page-item double text-right vert-center">
+			<h5>
+				<a href="tel:+16196558686" class="no-decor">(619) 655-8686</a><br>
+				<a href="mailto:edward.hofmeister@focus.org" class="no-decor">edward.hofmeister@focus.org</a>
+			</h5>
+		</div>
+	</div>
+</div>
