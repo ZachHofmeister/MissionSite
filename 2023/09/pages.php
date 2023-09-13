ff1282c635a076a502ba83438c6805591b4932de
ZachHofmeister <zachhof@me.com>
2023-09-13 11:21:58 -0600

    Made sep newsletter template

---
+++ 2023/09/pages.php
@@ -0,0 +1,166 @@
+<!-- Yours, O Lord, are the greatness, the power, the glory, the victory, and the majesty; for all that is in the heavens and on the earth is yours; yours is the kingdom, O Lord, and you are exalted as head above all. - 1 Chronicles 29:11 -->
+
+<!-- PAGE ONE -->
+<div class="page">
+	<!-- HEADER -->
+	<div class="page-section _10">
+		<div class="page-item vert-center">
+			<img class="contain no-lightbox" src="/2023/09/images/focus-logo.png" alt="FOCUS logo">
+		</div>
+		<div class="page-item vert-center text-center w6">
+			<h1 class="cardo">The Mission at Mesa</h1>
+			<h4 class="italic">Zach Hofmeister<span class="no-style"> | </span>Colorado Mesa University<span class="no-style"> | </span>September 2023</h4>
+		</div>
+		<div class="page-item vert-center">
+			<img class="contain no-lightbox" src="/2023/09/images/cmu-logo.png" alt="CMU logo">
+		</div>
+	</div>
+
+	<!-- MAIN IMAGE -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h100" src="/2023/08/images/team-retreat.jpeg" alt="" style="object-position: 50% 50%;">
+		</div>
+	</div>
+
+	<!-- PARAGRAPH / PHOTO -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h90" src="/2023/08/images/faith-fair.jpeg" alt="">
+			<p class="caption text-large">CAPTION</p>
+		</div>
+		<div class="page-item text-right">
+			<div class="box">
+				<h2 class="italic">Dear Friends,</h2>
+				<p class="text-large">Paragraph</p>
+			</div>
+		</div>
+	</div>
+
+	<!-- PARAGRAPH / PHOTO -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<h2>Fall Outreach</h2>
+			<p class="text-large">Paragraph</p>
+		</div>
+		<div class="page-item">
+			<img class="h90" src="/2023/08/images/club-fair.jpeg" alt="">
+			<p class="caption text-large">CAPTION</p>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE TWO -->
+<div class="page">
+	<!-- PARAGRAPH / PHOTO -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h90" src="/2023/08/images/squad-walking.jpeg" alt="" style="object-position: 50% 80%;">
+			<p class="caption text-large">CAPTION</p>
+		</div>
+		<div class="page-item">
+			<h2 class="text-right">Other Highlights</h2>
+			<ul class="text-large">
+				<li>...</li>
+			</ul>
+		</div>
+	</div>
+
+	<!-- PHOTOS -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h90" src="/2023/08/images/chapel-prayer.jpeg" alt="">
+			<p class="caption text-large">CAPTION</p>
+		</div>	
+		<div class="page-item">
+			<img class="h90" src="/2023/08/images/unmatched-game.jpeg" alt="">
+			<p class="caption text-large">CAPTION</p>
+		</div>
+	</div>
+
+	<!-- PRAYER REQUESTS / THANK YOU -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<h2>Prayer Requests</h2>
+			<ul class="text-large">
+				<li>For...</li>
+			</ul>
+		</div>
+		<div class="page-item text-large text-right">
+			<div class="box">
+				<h2 class="italic">THANK YOU!</h2>
+				<p class="text-large">
+					Thank you!
+				</p>
+				<p class="text-large">God bless you, Zach</p>
+			</div>
+		</div>
+	</div>
+	
+	<!-- FOOTER -->
+	<div class="page-section _10">
+		<div class="page-item double text-left vert-center">
+			<h4>
+				***REMOVED***<br>
+				Grand Junction, CO 81501
+			</h4>
+		</div>
+		<div class="page-item vert-center">
+			<a href="https://www.focus.org" target="_blank">
+				<img class="contain no-lightbox" src="/2023/08/images/focus-logo.png" alt="FOCUS logo">
+			</a>
+		</div>
+		<div class="page-item double text-right vert-center">
+			<h4>
+				<a href="tel:+16196558686" class="no-decor">(619) 655-8686</a><br>
+				<a href="mailto:edward.hofmeister@focus.org" class="no-decor">edward.hofmeister@focus.org</a>
+			</h4>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE THREE -->
+<div class="page">
+	<!-- HEADING -->
+	<div class="page-section _5">
+		<div class="page-item text-center center-center">
+			<h2>More Photos!</h2>
+		</div>
+	</div>
+	
+	<!-- PHOTOS-->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h80" src="/2023/08/images/first-day.jpeg" alt="">
+			<p class="caption text-large">CAPTION</p>
+		</div>
+		<div class="page-item">
+			<img class="h80" src="/2023/08/images/second-day.jpeg" alt="">
+			<p class="caption text-large">CAPTION</p>
+		</div>
+	</div>
+
+	<!-- PHOTOS-->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h80" src="/2023/08/images/nerf-1.jpeg" alt="" style="object-position: 50% 20%;">
+			<p class="caption text-large">CAPTION</p>
+		</div>
+		<div class="page-item">
+			<img class="h80" src="/2023/08/images/nerf-2.jpeg" alt="">
+			<p class="caption text-large">CAPTION</p>
+		</div>
+	</div>
+
+	<!-- PHOTOS-->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h80" src="/2023/08/images/nite-hike-1.jpeg" alt="">
+			<p class="caption text-large">CAPTION</p>
+		</div>
+		<div class="page-item">
+			<img class="h80" src="/2023/08/images/nite-hike-2.jpeg" alt="">
+			<p class="caption text-large">CAPTION</p>
+		</div>
+	</div>
+</div>
