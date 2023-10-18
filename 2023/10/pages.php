95560ff4c735392e6b49e9332a29b0c1b32b82f3
ZachHofmeister <zachhof@me.com>
2023-10-18 11:01:35 -0600

    started working on oct23

---
+++ 2023/10/pages.php
@@ -0,0 +1,123 @@
+<!-- Yours, O Lord, are the greatness, the power, the glory, the victory, and the majesty; for all that is in the heavens and on the earth is yours; yours is the kingdom, O Lord, and you are exalted as head above all. - 1 Chronicles 29:11 -->
+
+<!-- PAGE 1 -->
+<div class="page">
+	<!-- HEADER -->
+	<div class="page-section _10">
+		<div class="page-item vert-center">
+			<img class="contain no-lightbox" src="/2023/10/images/focus-logo.png" alt="FOCUS logo">
+		</div>
+		<div class="page-item vert-center text-center w6">
+			<h1 class="cardo">The Mission at Mesa</h1>
+			<h4 class="italic">Zach Hofmeister<span class="no-style"> | </span>Colorado Mesa University<span class="no-style"> | </span>October 2023</h4>
+		</div>
+		<div class="page-item vert-center">
+			<img class="contain no-lightbox" src="/2023/10/images/cmu-logo.png" alt="CMU logo">
+		</div>
+	</div>
+
+	<!-- MAIN IMAGE -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h100" src="/2023/10/images/st-francis.jpeg" alt="" style="object-position: 50% 70%;" data-caption="Myself, Olivia, Jacky, Brian, and Ally celebrating St. Francis' feast day on October 4th (my patron saint!)">
+		</div>
+	</div>
+
+	<!-- INTRO / PHOTO -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="photo-item h100" src="/2023/10/images/st-francis.jpeg" alt="">
+		</div>
+		<div class="page-item text-right">
+			<div class="box">
+				<h2 class="italic">Dear Friends,</h2>
+				<p class="text-large">I hope that you had a great September! Thank you for being a part of this mission! This month, God has blessed me with getting to grow closer to our student leaders and the men in my Bible study. I'm excited to share about it with you!</p>
+			</div>
+		</div>
+	</div>
+
+	<!-- BIBLE STUDIES + PHOTO -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<h3>Bible Studies</h3>
+			<p class="text-large">At the start of September we invited some students to come camping in Moab for a weekend. It was a great opportunity to get to know some of them better, hear about their journeys in faith, and be in awe of God's beautiful creation together!</p>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2023/10/images/st-francis.jpeg" alt="">
+			<p class="caption text-large">Caption</p>
+		</div>
+		
+	</div>
+</div>
+
+<!-- PAGE 2 -->
+<div class="page">
+	<!-- DISCIPLESHIP + PHOTO -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2023/10/images/st-francis.jpeg" alt="">
+			<p class="caption text-large">Caption</p>
+		</div>	
+		<div class="page-item text-right">
+			<h3>Discipleship</h3>
+			<p class="text-large">Paragraph</p>
+		</div>
+	</div>
+
+	<!-- HIGHLIGHTS + PHOTO -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<h3>Other Highlights</h3>
+			<ul class="text-large">
+				<li>The Mesa Catholic Halloween party!</li>
+			</ul>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2023/10/images/st-francis.jpeg" alt="">
+			<p class="caption text-large">Caption</p>
+		</div>	
+	</div>
+
+	<!-- PRAYER REQUESTS / THANK YOU -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<h2>Prayer Requests</h2>
+			<ul class="text-large">
+				<li>For the men in my Bible study: Gabe, Drew, Robert, Elijah, Dylan, Nick, and Barrett and Tyler who I am inviting</li>
+				<li>For our Gospel invitations this month</li>
+				<li>For our <a href="https://seek.focus.org/" target="_blank">SEEK</a> and mission trip invitations</li>
+				<li>For boldness in outreach</li>
+			</ul>
+		</div>
+		<div class="page-item text-large text-right">
+			<div class="box">
+				<h2 class="italic">THANK YOU!</h2>
+				<p class="text-large">
+					Thank you for making our mission at Colorado Mesa possible by your prayers and support for me! I am so blessed to be on mission with you!
+				</p>
+				<p class="text-large">In Christ Jesus, Zach</p>
+			</div>
+		</div>
+	</div>
+	
+	<!-- FOOTER -->
+	<div class="page-section _10">
+		<div class="page-item double text-left vert-center">
+			<h4>
+				***REMOVED***<br>
+				Grand Junction, CO 81501
+			</h4>
+		</div>
+		<div class="page-item vert-center">
+			<a href="https://www.focus.org" target="_blank">
+				<img class="contain no-lightbox" src="/2023/10/images/focus-logo.png" alt="FOCUS logo">
+			</a>
+		</div>
+		<div class="page-item double text-right vert-center">
+			<h4>
+				<a href="tel:+16196558686" class="no-decor">(619) 655-8686</a><br>
+				<a href="mailto:edward.hofmeister@focus.org" class="no-decor">edward.hofmeister@focus.org</a>
+			</h4>
+		</div>
+	</div>
+</div>
