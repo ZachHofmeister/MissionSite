86692999b51600a8e56941bdb22803c0abd637f9
ZachHofmeister <zachhof@me.com>
2023-11-02 00:32:53 -0600

    finished oct newsletter

--- 2023/10/pages.php
+++ 2023/10/pages.php
@@ -26,74 +26,122 @@
 	<!-- INTRO / PHOTO -->
 	<div class="page-section _30">
 		<div class="page-item">
-			<img class="photo-item h100" src="/2023/10/images/st-francis.jpeg" alt="">
+			<img class="photo-item h80" src="/2023/10/images/game-night.jpeg" alt="" data-caption=". Every week I host a game night for my Bible study guys and anyone else who is free!">
+			<p class="caption text-large">Game night with Drew, Gabe, Elijah, Robert, Ace, Bridget, and Ally</p>
 		</div>
 		<div class="page-item text-right">
 			<div class="box">
 				<h2 class="italic">Dear Friends,</h2>
-				<p class="text-large">I hope that you had a great September! Thank you for being a part of this mission! This month, God has blessed me with getting to grow closer to our student leaders and the men in my Bible study. I'm excited to share about it with you!</p>
+				<p class="text-large">I hope that you have had such a blessed October! The semester is flying by so fast, and there is so much happening in our mission! Here are a few of the things we've been doing this past month!</p>
 			</div>
 		</div>
 	</div>
 
-	<!-- BIBLE STUDIES + PHOTO -->
+	<!-- The Gospel and Discipleship + photo -->
 	<div class="page-section _30">
 		<div class="page-item">
-			<h3>Bible Studies</h3>
-			<p class="text-large">At the start of September we invited some students to come camping in Moab for a weekend. It was a great opportunity to get to know some of them better, hear about their journeys in faith, and be in awe of God's beautiful creation together!</p>
+			<h3>The Gospel and Discipleship</h3>
+			<p class="text-large">
+				This month in Bible study we finished walking through The Crux, which is focused on presenting the Gospel. The guys have been very receptive to the call to make Christ the center of their lives, and all of them are desiring a deeper relationship with Him.
+			</p>
 		</div>
 		<div class="page-item">
-			<img class="photo-item h80" src="/2023/10/images/st-francis.jpeg" alt="">
-			<p class="caption text-large">Caption</p>
+			<img class="h80" src="/2023/10/images/bstud-catan.jpeg" alt="">
+			<p class="caption text-large">Playing Catan with Barrett, Nick, and Drew after our Bible study</p>
 		</div>
-		
 	</div>
 </div>
 
 <!-- PAGE 2 -->
 <div class="page">
-	<!-- DISCIPLESHIP + PHOTO -->
+	<!-- G&D continued -->
 	<div class="page-section _30">
+		<div class="page-item photo-stack">
+			<div class="photo-item">
+				<img class="h80" src="/2023/10/images/seek-party.jpeg" alt="">
+				<p class="caption text-large">We had a great turnout for our SEEK info party!</p>
+			</div>
+		</div>
+		<div class="page-item text-right">
+			<p class="text-large">
+			So far, I've individually shared the Gospel with Gabe and Drew, and started walking with them in discipleship! We've been discussing <a href="https://www.biblegateway.com/passage/?search=Acts+2%3A42&version=RSVCE" target="_blank">Acts 2:42</a>, and how the Lord is calling us to a deeper relationship with him through the apostles' teaching, fellowship, the sacraments, and prayer.
+			</p>
+		</div>
+	</div>
+
+	<!-- Seek Week -->
+	<div class="page-section _70">
 		<div class="page-item">
-			<img class="photo-item h80" src="/2023/10/images/st-francis.jpeg" alt="">
-			<p class="caption text-large">Caption</p>
+			<h3>SEEK Week</h3>
+			<p class="text-large">
+				This month we've also started inviting all our students to <a href="https://seek.focus.org" target="_blank">SEEK</a>, the annual conference FOCUS hosts to bring thousands of college students together each winter! Attending SEEK each year of college always proved to be a point of deeper conversion for me, and played a big role in my hearing Jesus' call to serve as a missionary. The peak of our invitations was an info party we threw at the Newman house where we shared about our experiences and helped them get signed up! We have 18 students signed up already, and our goal is to have 30 coming with us! I'm so excited to continue inviting students and helping them to be able to attend, and to see how Jesus will work in our time there!
+			</p>
+		</div>
+		<div class="page-item photo-stack">
+			<div class="photo-item">
+				<img class="h70" src="/2023/10/images/seek-testimonies.jpeg" alt="" style="object-position: 50% 20%;">
+				<p class="caption text-large">Matthew and Brenna shared their testimonies from past SEEK's</p>
+			</div>
+			<div class="photo-item">
+				<img class="h70" src="/2023/10/images/seek-emcees.jpeg" alt="">
+				<p class="caption text-large">Ally and I were the emcees for the night!</p>
+			</div>
+			<div class="photo-item">
+				<img class="h70" src="/2023/10/images/seek-going.jpeg" alt="">
+				<p class="caption text-large">We have 18 students signed up for SEEK so far!</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE 3 -->
+<div class="page">
+	<!-- Gospel Highlight + photo -->
+	<div class="page-section _35">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2023/10/images/pumpkin-carving.jpeg" alt="">
+			<p class="caption text-large">Olivia, Victoria, Miranda, Erin, and Jacky at our pumpkin carving night</p>
 		</div>	
 		<div class="page-item text-right">
-			<h3>Discipleship</h3>
-			<p class="text-large">Paragraph</p>
+			<h3>Gospel Highlight: Luke 12:22-34</h3>
+			<p class="text-large">
+				A Gospel passage I kept coming back to in prayer this month was <a href="https://www.biblegateway.com/passage/?search=Luke+12%3A22-34&version=NABRE" target="_blank">Luke 12:22-34</a>. I was especially touched by the gentle way that Jesus invites His disciples to trust in Him more, and have been seeking to give more of my trust to His plan over mine. I invite you to spend some time with Him in this passage too!
+			</p>
 		</div>
 	</div>
 
 	<!-- HIGHLIGHTS + PHOTO -->
-	<div class="page-section _30">
+	<div class="page-section _25">
 		<div class="page-item">
 			<h3>Other Highlights</h3>
 			<ul class="text-large">
-				<li>The Mesa Catholic Halloween party!</li>
+				<li>The Mesa Catholic Halloween party</li>
+				<li>Singing spontaneous karaoke after the SEEK party</li>
+				<li>Going to confession with four of my Bible study men</li>
+				<li>Meeting Fr. Carl, an awesome priest who came and spoke to us about the realities of spiritual warfare</li>
 			</ul>
 		</div>
 		<div class="page-item">
-			<img class="photo-item h80" src="/2023/10/images/st-francis.jpeg" alt="">
-			<p class="caption text-large">Caption</p>
+			<img class="photo-item h80" src="/2023/10/images/air-force.jpeg" alt="" data-caption=" Pictured is me, Pete, Rod, Emile, and Ben (one of the new missionaries) at nearly 14,000 feet! They are doing well!">
+			<p class="caption text-large">I went camping with some of my Air Force Academy friends this month!</p>
 		</div>	
 	</div>
 
 	<!-- PRAYER REQUESTS / THANK YOU -->
 	<div class="page-section _30">
 		<div class="page-item">
-			<h2>Prayer Requests</h2>
+			<h2>Please Pray...</h2>
 			<ul class="text-large">
-				<li>For the men in my Bible study: Gabe, Drew, Robert, Elijah, Dylan, Nick, and Barrett and Tyler who I am inviting</li>
-				<li>For our Gospel invitations this month</li>
-				<li>For our <a href="https://seek.focus.org/" target="_blank">SEEK</a> and mission trip invitations</li>
-				<li>For boldness in outreach</li>
+				<li>For my Bible study men: Gabe, Drew, Robert, Elijah, Nick, Barrett, Dylan, and Tyler</li>
+				<li>For our students and our team to grow in love of prayer and intimacy with Jesus</li>
+				<li>For greater surrender to the Lord's will for my life and on mission</li>
 			</ul>
 		</div>
 		<div class="page-item text-large text-right">
 			<div class="box">
 				<h2 class="italic">THANK YOU!</h2>
 				<p class="text-large">
-					Thank you for making our mission at Colorado Mesa possible by your prayers and support for me! I am so blessed to be on mission with you!
+					Your prayers and support are the reason that I'm able to be on mission for Jesus at Colorado Mesa. Thank you so much for that great gift to me and our students!
 				</p>
 				<p class="text-large">In Christ Jesus, Zach</p>
 			</div>
@@ -118,6 +166,66 @@
 				<a href="tel:+16196558686" class="no-decor">(619) 655-8686</a><br>
 				<a href="mailto:edward.hofmeister@focus.org" class="no-decor">edward.hofmeister@focus.org</a>
 			</h4>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE 4 -->
+<div class="page">
+	<div class="page-section _5">
+		<div class="page-item center-center">
+			<h2>More Photos!</h2>
+		</div>
+	</div>
+	<!-- photos -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2023/10/images/talent-show.jpeg" alt="">
+			<p class="caption text-large">For outreach on campus we invited students to show off their talents</p>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2023/10/images/fr-carl-guitar.jpeg" alt="">
+			<p class="caption text-large">Fr. Carl, who spoke to us about spiritual warfare, is also an amazing guitarist</p>
+		</div>
+	</div>
+	<!-- photos -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2023/10/images/halloween-team.jpeg" alt="">
+			<p class="caption text-large">My teammates and I dressed as a 70's rock band for Halloween</p>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2023/10/images/halloween-costumes.jpeg" alt="">
+			<p class="caption text-large">Matthew, Drew, and Gabe at the Halloween party</p>
+		</div>
+	</div>
+	<!-- photos -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2023/10/images/karaoke-bov.jpeg" alt="">
+			<p class="caption text-large">Bridget, Olivia, and Victoria came as the Schuyler sisters and sang their <a href="https://www.youtube.com/watch?v=POHVE9uCNAY" target="_blank">song from Hamilton</a> for karaoke</p>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2023/10/images/karaoke-ka.jpeg" alt="">
+			<p class="caption text-large">Kylee and Allison singing karaoke</p>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE 5 -->
+<div class="page _50">
+	<!-- Header -->
+	<div class="page-section _15">
+		<div class="page-item center-center">
+			<h3>FOCUS National News</h3>
+			<p class="text-large">Thank you for being with me on this mission!</p>
+		</div>
+	</div>
+	
+	<!-- Image -->
+	<div class="page-section _85">
+		<div class="page-item">
+			<a href="https://focus.org/stats" target="_blank"><img class="photo-item no-lightbox h100" src="/2023/10/images/oct-news-insert.jpeg" alt=""></a>
 		</div>
 	</div>
 </div>
