2de08ea50dea0734a04a0f443aaf7161a8c747c3
ZachHofmeister <zachhof@me.com>
2023-12-07 23:32:26 -0700

    finished nov newsletter

---
+++ 2023/11/head.php
@@ -0,0 +1,2 @@
+<title>November 2023 Update</title>
+<link rel="stylesheet" type="text/css" href="/2023/11/colors.css">
---
+++ 2023/11/pages.php
@@ -0,0 +1,191 @@
+<!-- Yours, O Lord, are the greatness, the power, the glory, the victory, and the majesty; for all that is in the heavens and on the earth is yours; yours is the kingdom, O Lord, and you are exalted as head above all. - 1 Chronicles 29:11 -->
+
+<!-- PAGE 1 -->
+<div class="page">
+	<!-- HEADER -->
+	<div class="page-section _10">
+		<div class="page-item vert-center">
+			<img class="contain no-lightbox" src="/2023/11/images/focus-logo.png" alt="FOCUS logo">
+		</div>
+		<div class="page-item vert-center text-center w6">
+			<h1 class="cardo">The Mission at Mesa</h1>
+			<h4 class="italic">Zach Hofmeister<span class="no-style"> | </span>Colorado Mesa University<span class="no-style"> | </span>November 2023</h4>
+		</div>
+		<div class="page-item vert-center">
+			<img class="contain no-lightbox" src="/2023/10/images/cmu-logo.png" alt="CMU logo">
+		</div>
+	</div>
+
+	<!-- MAIN IMAGE -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h100" src="/2023/11/images/bowling.jpeg" alt="" style="object-position: 50% 70%;" data-caption="A group of us went bowling after men's and women's ministry one night!">
+		</div>
+	</div>
+
+	<!-- INTRO / PHOTO -->
+	<div class="page-section _30">
+		<div class="page-item text-right">
+			<div class="box">
+				<h2 class="italic">Dear Friends,</h2>
+				<p class="text-large">
+					I hope you had a great November and a happy Thanksgiving! I am so thankful for this mission that we get to be on together! This month has been filled with many blessings that I'm excited to tell you about.
+				</p>
+			</div>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2023/11/images/interview-weekend.jpeg" alt="" style="object-position: 50% 20%">
+			<p class="caption text-large">The group of applicants I got to support at a FOCUS interview weekend</p>
+		</div>
+	</div>
+
+	<!-- Leadership Study -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h80" src="/2023/11/images/leaders-study.jpeg" alt="">
+			<p class="caption text-large">Fr. Abraham visited our student leadership Bible study one night</p>
+		</div>
+		<div class="page-item">
+			<h3>Student Testimonies</h3>
+			<p class="text-large">
+				In our leadership Bible study, we have been teaching our students about knowing and sharing their testimonies and the Gospel with others! It was a joy to hear them share how the Lord has been calling them to Himself and to see their desire to witness to their faith!
+			</p>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE 2 -->
+<div class="page">
+	<!-- Discipleship and Outreach Pics -->
+	<div class="page-section _50">
+		<div class="page-item text-right">
+			<h3>Discipleship Update</h3>
+			<p class="text-large">
+				This month, I shared the Gospel individually with Robert and Elijah and invited them into discipleship! In our recent weekly meetings I invited them to pray for 30 minutes every day, and we've been walking through how to build that habit together. Gabe and Drew received the same challenge and have been doing well in their prayer commitment, and I'm also very excited that they will be joining us at the SEEK conference over break! Please keep them all in your prayers!
+			</p>
+		</div>
+		<div class="page-item photo-stack">
+			<div class="photo-item">
+				<img class="h70" src="/2023/11/images/outreach-1.jpeg" alt="">
+				<p class="caption text-large">We invited students on campus to help us make bags for the homeless</p>
+			</div>
+			<div class="photo-item">
+				<img class="h70" src="/2023/11/images/outreach-2.jpeg" alt="">
+				<p class="caption text-large">Later that week we visited a local park and handed them out!</p>
+			</div>
+		</div>
+	</div>
+
+	<!-- Bible Study and Christmas pics -->
+	<div class="page-section _50">
+		<div class="page-item photo-stack">
+			<div class="photo-item">
+				<img class="h70" src="/2023/11/images/decor-1.jpeg" alt="">
+				<p class="caption text-large">Having some fun putting up Christmas decorations</p>
+			</div>
+			<div class="photo-item">
+				<img class="h70" src="/2023/11/images/decor-2.jpeg" alt="">
+				<p class="caption text-large">Everyone was very entertained by Gabe becoming a Christmas tree</p>
+			</div>
+		</div>
+		<div class="page-item">
+			<h3>Bible Study</h3>
+			<p class="text-large">
+				My Bible study started walking through <a href="https://focusequip.org/salvation-history/" target="_blank">the Story of Salvation</a> study this month. The focus of the study is on God's relationship with man as seen in the stories of multiple biblical figures. So far, we have discussed the lives of Adam, Noah, and Abraham, and compared their journeys with God to our own. My favorite discussion so far has been reading about Abraham's journey from partial to complete faith in God, and talking about how we can take steps towards more complete faith also.
+			</p>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE 3 -->
+<div class="page">
+	<!-- Advent + Keegan -->
+	<div class="page-section _35">
+		<div class="page-item text-right">
+			<h3>Thanksgiving Break</h3>
+			<p class="text-large">
+				For Thanksgiving break I stayed in Grand Junction while my teammates and most of our students went home. My highlight of the break was having a really good conversation over breakfast with Keegan and going to pray with him afterwards. I also got to celebrate my birthday with some of my friends who stayed which was so fun!
+			</p>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2023/11/images/birthday-keegan.jpeg" alt="">
+			<p class="caption text-large">Keegan and I went target shooting for my birthday</p>
+		</div>
+	</div>
+
+	<!-- HIGHLIGHTS + Melvin -->
+	<div class="page-section _25">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2023/11/images/melvin.jpeg" alt="">
+			<p class="caption text-large">One of our students gave Brian and I a cat... meet Melvin!</p>
+		</div>
+		<div class="page-item">
+			<h3>Other Highlights</h3>
+			<ul class="text-large">
+				<li>Our mission trip party, where we invited students to come to Peru with us for their spring break!</li>
+				<li>Watching the new Hunger Games movie with students</li>
+				<li>The first snow of the season!</li>
+			</ul>
+		</div>
+	</div>
+
+	<!-- PRAYER REQUESTS / THANK YOU -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<h2>Please Pray...</h2>
+			<ul class="text-large">
+				<li>For our students to remain faithful and growing in their relationships with the Lord over break</li>
+				<li>For our students still considering coming on the mission trip: Nick, Kylee, Robert, and Elijah</li>
+				<li>For the Lord to prepare the hearts of our SEEK participants for the many graces the will encounter there</li>
+			</ul>
+		</div>
+		<div class="page-item text-large">
+			<div class="box">
+				<h2 class="italic">THANK YOU!</h2>
+				<p class="text-large">
+					Thank you for being a part of this mission! It is such a blessing to witness and share the graces the Lord is working here with you each month!
+				</p>
+				<p class="text-large">In Christ Jesus, Zach</p>
+			</div>
+		</div>
+	</div>
+	
+	<!-- FOOTER -->
+	<div class="page-section _10">
+		<div class="page-item double text-left vert-center">
+			<h4>
+				***REMOVED***<br>
+				Grand Junction, CO 81501
+			</h4>
+		</div>
+		<div class="page-item vert-center">
+			<a href="https://www.focus.org" target="_blank">
+				<img class="contain no-lightbox" src="/2023/10/images/focus-logo.png" alt="FOCUS logo">
+			</a>
+		</div>
+		<div class="page-item double text-right vert-center">
+			<h4>
+				<a href="tel:+16196558686" class="no-decor">(619) 655-8686</a><br>
+				<a href="mailto:edward.hofmeister@focus.org" class="no-decor">edward.hofmeister@focus.org</a>
+			</h4>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE 4 -->
+<div class="page _50">
+	<!-- Header -->
+	<div class="page-section _15">
+		<div class="page-item center-center">
+			<h3>FOCUS National News</h3>
+			<p class="text-large">Thank you for being with me on this mission!</p>
+		</div>
+	</div>
+	
+	<!-- FOCUS National News -->
+	<div class="page-section _85">
+		<div class="page-item">
+			<a href="https://focus.org/preparetheway" target="_blank"><img class="photo-item no-lightbox h100" src="/2023/11/images/focus-news-november.png" alt=""></a>
+		</div>
+	</div>
+</div>
