cd4845bacab5a25e08961f8c38588847dd15343b
ZachHofmeister <zachhof@me.com>
2024-01-31 14:51:23 -0700

    finished jan24 newsletter

---
+++ 2024/01/pages.php
@@ -0,0 +1,209 @@
+<!-- Yours, O Lord, are the greatness, the power, the glory, the victory, and the majesty; for all that is in the heavens and on the earth is yours; yours is the kingdom, O Lord, and you are exalted as head above all. - 1 Chronicles 29:11 -->
+
+<!-- PAGE 1 -->
+<div class="page">
+	<!-- HEADER -->
+	<div class="page-section _10">
+		<div class="page-item vert-center">
+			<img class="contain no-lightbox" src="/2024/01/images/focus-logo.png" alt="FOCUS logo">
+		</div>
+		<div class="page-item vert-center text-center w6">
+			<h1 class="cardo">The Mission at Mesa</h1>
+			<h4 class="italic">Zach Hofmeister<span class="no-style"> | </span>Colorado Mesa University<span class="no-style"> | </span>January 2024</h4>
+		</div>
+		<div class="page-item vert-center">
+			<img class="contain no-lightbox" src="/2023/10/images/cmu-logo.png" alt="CMU logo">
+		</div>
+	</div>
+
+	<!-- MAIN IMAGE -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h100" src="/2024/01/images/seek-arch.jpeg" alt="" style="object-position: 50% 50%;" data-caption="Our group at the St. Louis Arch on the last day of SEEK24!">
+		</div>
+	</div>
+
+	<!-- INTRO / PHOTO -->
+	<div class="page-section _30">
+		<div class="page-item text-right">
+			<div class="box">
+				<h2 class="italic">Dear Friends,</h2>
+				<p class="text-large">
+					Happy New Year! After a great break with my family, I started my year with the SEEK conference in St. Louis, followed by returning to Grand Junction, planning the semester, and starting spring outreach on campus!
+				</p>
+			</div>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/01/images/seek-fr-mike.jpeg" alt="" style="object-position: 50% 50%">
+			<p class="caption text-large">Some of us got to meet Fr. Mike Schmitz, one of our favorite speakers!</p>
+		</div>
+	</div>
+
+	<!-- SEEK -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h80" src="/2024/01/images/seek-men.jpeg" alt="" data-caption=". Jorge, Drew, Gabe, Adrian, and me.">
+			<p class="caption text-large">Some of our guys as we wait for the third men's talk to start</p>
+		</div>
+		<div class="page-item">
+			<h3>SEEK</h3>
+			<p class="text-large">
+				It was so good to experience SEEK anew through the eyes of our students! I spent most of my time with the men going to talks they were interested in, but it was also so freeing to break away occasionally to spend time with Jesus in Adoration and prayer.
+			</p>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE 2 -->
+<div class="page">
+	<!-- SEEK continued -->
+	<div class="page-section _33">
+		<div class="page-item text-right">
+			<p class="text-large">
+				Through the talks, my conversations, and my prayer at SEEK this year, the Lord has sparked a fire in my heart to reach the men of our campus through the message of authentic masculinity. God has given men the great opportunity to become living images of the Father in their love, and I am so excited to explore and share this more!
+			</p>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/01/images/seek-keegan-liv.jpeg" alt="" data-caption=". Bonus: behind us is the FOCUS supporter area where they'll make you a free latte - come join us in Salt Lake City for SEEK25 next year!">
+			<p class="caption text-large">Hanging out with Keegan and Liv in a line to meet one of the speakers</p>
+		</div>
+	</div>
+
+	<!-- Outreach -->
+	<div class="page-section _33">
+		<div class="page-item">
+			<img class="photo-item h100" src="/2024/01/images/campus-tabling.jpeg" alt="" data-caption="I haven't found a better way to meet people than by offering them a free donut, but it's surprising how many people have the willpower to resist">
+		</div>
+		<div class="page-item">
+			<h3>Outreach and Welcome Week</h3>
+			<p class="text-large">
+				The Saturday before classes began there was a small club fair, so we brought donuts and event flyers to meet and invite new students. My highlight this day was running into Minhtam, a friend of Gabe and Drew I'd met once last semester, and inviting him to a game night that week - he came!
+			</p>
+		</div>
+	</div>
+
+	<!-- Race -->
+	<div class="page-section _33">
+		<div class="page-item text-right">
+			<h3>Race Day</h3>
+			<p class="text-large">
+				Brian and Ally entered a race in Moab the next Saturday, so we got a group together to drive down and cheer for them! This is all of us at the finish line.
+			</p>
+			<p class="text-large">
+				<i>Do you not know that in a race all the runners compete, but only one receives the prize? So run that you may obtain it.</i><br> - 1 Corinthians 9:24
+			</p>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h100" src="/2024/01/images/race-day.jpeg" alt="" data-caption="Brian ran a 50k and Ally ran a half-marathon!">
+		</div>
+	</div>
+</div>
+
+<!-- PAGE 3 -->
+<div class="page">
+	<!-- Bday -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="photo-item h100" src="/2024/01/images/allys-bday.jpeg" alt="" data-caption="Olivia, Brenna, Jacky, Ally, Allison, me, and Brian">
+		</div>
+		<div class="page-item">
+			<h3>Ally's Birthday</h3>
+			<p class="text-large">
+				It was my teammate Ally's birthday this past Sunday, so after Mass we went to breakfast together with some of our students. It was certainly all the more joyful for being with Jesus in Mass right before, which is such a powerful way we can witness to the world!
+			</p>
+		</div>
+	</div>
+
+	<!-- I'm excited for -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<h3>This semester, I'm excited to...</h3>
+			<ul class="text-large">
+				<li>Host Saturday night confessions + dinner for the men with Brian</li>
+				<li>Start Upper Room - monthly meetings with all our disciples to encourage them in mission on campus</li>
+				<li>Go on our mission trip to Peru in March</li>
+			</ul>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/01/images/game-night.jpeg" alt="" style="object-position: 50% 30%;">
+			<p class="caption text-large">A great candid moment from game night at the Newman house</p>
+		</div>
+	</div>
+
+	<!-- PRAYER REQUESTS / THANK YOU -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<h2>Please Pray...</h2>
+			<ul class="text-large">
+				<li>For the new students we are meeting and inviting to Bible studies</li>
+				<li>For our disciples, that they may grow in the desire to share their faith with their peers</li>
+				<li>For my invitations to men to come on our mission trip</li>
+				<li>For my discernment about staying on mission with FOCUS next year</li>
+			</ul>
+		</div>
+		<div class="page-item text-large">
+			<div class="box">
+				<h2 class="italic">THANK YOU!</h2>
+				<p class="text-large">
+					You are making a difference not only in my life and the lives of our students, but also in those that will come to know Jesus through them. Thank you for making the Great Commission possible!
+				</p>
+				<p class="text-large text-right">In His Love, Zach</p>
+			</div>
+		</div>
+	</div>
+	
+	<!-- FOOTER -->
+	<div class="page-section _10">
+		<div class="page-item double text-left vert-center">
+			<h4>
+				***REMOVED***<br>
+				Grand Junction, CO 81501
+			</h4>
+		</div>
+		<div class="page-item vert-center">
+			<a href="https://www.focus.org" target="_blank">
+				<img class="contain no-lightbox" src="/2023/10/images/focus-logo.png" alt="FOCUS logo">
+			</a>
+		</div>
+		<div class="page-item double text-right vert-center">
+			<h4>
+				<a href="tel:+16196558686" class="no-decor">(619) 655-8686</a><br>
+				<a href="mailto:edward.hofmeister@focus.org" class="no-decor">edward.hofmeister@focus.org</a>
+			</h4>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE 4 -->
+<div class="page">
+	<!-- Header -->
+	<div class="page-section _5">
+		<div class="page-item center-center">
+			<h3>FOCUS National News</h3>
+		</div>
+	</div>
+	<!-- FOCUS National News -->
+	<div class="page-section _50">
+		<div class="page-item">
+			<a href="https://seekreplay.com" target="_blank"><img class="photo-item no-lightbox h100" src="/2024/01/images/news-jan-seek.jpeg" alt=""></a>
+		</div>
+	</div>
+	<!-- Header -->
+	<div class="page-section _5">
+		<div class="page-item center-center">
+			<p class="text-large text-center">FOCUS is making all the amazing SEEK24 talks available for free! I would especially recommend the keynote by Msgr. Shea.</p>
+		</div>
+	</div>
+	<!-- FOCUS National News -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<a href="https://focusequip.org/lent24" target="_blank"><img class="photo-item no-lightbox h100" src="/2024/01/images/news-jan-bstud.png" alt=""></a>
+		</div>
+	</div>
+	<!-- Header -->
+	<div class="page-section _10">
+		<div class="page-item center-center">
+			<p class="text-large text-center">This lent, would you like to grow in your knowledge of scripture? Curtis Martin and Dr. Edward Sri, co-founders of FOCUS, will be leading a lenten Bible study beginning February 14th!</p>
+		</div>
+	</div>
+</div>
