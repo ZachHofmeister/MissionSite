052f92575f90b1742f4b86775b467da041d38686
ZachHofmeister <zachhof@me.com>
2024-03-06 11:04:39 -0700

    started getting nl template ready for march

---
+++ 2024/03/pages.php
@@ -0,0 +1,192 @@
+<!-- Yours, O Lord, are the greatness, the power, the glory, the victory, and the majesty; for all that is in the heavens and on the earth is yours; yours is the kingdom, O Lord, and you are exalted as head above all. - 1 Chronicles 29:11 -->
+
+<!-- PAGE 1 -->
+<div class="page">
+	<!-- HEADER -->
+	<div class="page-section _10">
+		<div class="page-item vert-center">
+			<img class="contain no-lightbox" src="/2024/03/images/focus-logo.png" alt="FOCUS logo">
+		</div>
+		<div class="page-item vert-center text-center w6">
+			<h1 class="cardo">The Mission at Mesa</h1>
+			<h4 class="italic">Zach Hofmeister<span class="no-style"> | </span>Colorado Mesa University<span class="no-style"> | </span>March 2024</h4>
+		</div>
+		<div class="page-item vert-center">
+			<img class="contain no-lightbox" src="/2023/10/images/cmu-logo.png" alt="CMU logo">
+		</div>
+	</div>
+
+	<!-- MAIN IMAGE -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h100" src="/2024/03/images/upperroom.jpeg" alt="" style="object-position: 50% 45%;" data-caption="">
+		</div>
+	</div>
+
+	<!-- INTRO / PHOTO -->
+	<div class="page-section _30">
+		<div class="page-item text-right">
+			<div class="box">
+				<h2 class="italic">Dear Friends,</h2>
+				<p class="text-large">
+					Text
+				</p>
+			</div>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/03/images/seek-party.jpeg" alt="" style="object-position: 50% 50%">
+			<p class="caption text-large">Caption</p>
+		</div>
+	</div>
+
+	<!-- Upper Room -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h80" src="/2024/03/images/hot-takes.jpeg" alt="" >
+			<p class="caption text-large">Caption</p>
+		</div>
+		<div class="page-item">
+			<h3>Upper Room</h3>
+			<p class="text-large">
+				My favorite highlight this month was our first ever Upper Room at Mesa! Modeled after Jesus gathering with the Apostles, at Upper Room we gather all our disciples together and form them in how to be more intentionally on mission on campus (see top image). 
+			</p>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE 2 -->
+<div class="page">
+	<!-- Valentine -->
+	<div class="page-section _33">
+		<div class="page-item">
+			<img class="photo-item h100" src="/2024/03/images/early-valentine.jpeg" alt="" >
+		</div>
+		<div class="page-item">
+			<h3>(Early) St. Valentine's Day</h3>
+			<p class="text-large">
+				Since Valentine's Day overlapped with Ash Wednesday this year, we celebrated it early by giving out roses with mass info for the next day! The students were really grateful for the roses, and people started coming to us because of their friends telling them too!
+			</p>
+		</div>
+	</div>
+
+	<!-- Ash Wednesday -->
+	<div class="page-section _33">
+		<div class="page-item text-right">
+			<h3>Ash Wednesday</h3>
+			<p class="text-large">
+				On Ash Wednesday we had two masses on campus, and manned a table all day to tell people about them. We answered a lot of people on campus confused about why we were calling Valentine's day Ash Wednesday or wondering what it was about, and we had about 80 students total come to mass!
+			</p>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h100" src="/2024/03/images/ash-mass.jpeg" alt="" >
+		</div>
+	</div>
+
+	<!-- Men's Night -->
+	<div class="page-section _33">
+		<div class="page-item">
+			<img class="photo-item h100" src="/2024/03/images/pit-stop.jpeg" alt="" data-caption="The bolts on one of the brakes had mysteriously fallen off - it is a miracle that she drove it to us without losing brake pressure or causing any damage to the car!">
+		</div>
+		<div class="page-item">
+			<h3>Men's Night</h3>
+			<p class="text-large">
+				We've had two of our biweekly men's nights so far, and they've been very successful in uniting the men in our community. We start the nights with confession and prayer, and then have a fun activity! This past week, Nadine had car trouble and we banded together to help, which was a fun surprise!
+			</p>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE 3 -->
+<div class="page">
+	<!-- Retreat -->
+	<div class="page-section _30">
+		<div class="page-item text-right">
+			<h3>Awakening Retreat</h3>
+			<p class="text-large">
+				At the end of the month we went on a weekend retreat led by our student leaders. They did a good job giving talks and sharing their testimonies, and Fr. Carl, a young priest, came to give us Mass, Adoration, and confession! It was great that most of my Bible study came!
+			</p>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/03/images/retreat-bstud.jpeg" alt="" data-caption="">
+			<p class="caption text-large">Caption</p>
+		</div>
+	</div>
+
+	<!-- Other highlights -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/03/images/retreat-prayer.jpeg" alt="" style="object-position: 50% 30%;">
+			<p class="caption text-large">Caption</p>
+		</div>
+		<div class="page-item">
+			<h3>Other Highlights</h3>
+			<ul class="text-large">
+				<li>Seeing real Christian brotherhood forming in my Bible study</li>
+				<li>Also, playing volleyball against them</li>
+				<li>Inviting Barrett into discipleship!</li>
+				<li>Celebrating Brian's birthday</li>
+				<li>Getting to present about St. Valentine's day and what love really is</li>
+			</ul>
+		</div>
+	</div>
+
+	<!-- PRAYER REQUESTS / THANK YOU -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<h2>Please Pray...</h2>
+			<ul class="text-large">
+				<li>For our mission trip to Peru, March 14th-24th! Pray for Jacky, Nadine, Kylee, John, Tessa, Nick, myself and the rest of the students</li>
+				<li>For our disciples to persevere in daily prayer</li>
+				<li>For Christian, Santiago, and Minhtam who I'm inviting to Bible study</li>
+				<li>For Ace's friend Andy who passed away recently</li>
+			</ul>
+		</div>
+		<div class="page-item text-large">
+			<div class="box">
+				<h2 class="italic">THANK YOU!</h2>
+				<p class="text-large">
+					Thank you for supporting me in this mission through your prayers and generous giving! It is such a blessing to be part of the work Jesus is doing on our campus alongside you!
+				</p>
+				<p class="text-large text-right">All Glory to Jesus, Zach</p>
+			</div>
+		</div>
+	</div>
+	
+	<!-- FOOTER -->
+	<div class="page-section _10">
+		<div class="page-item double text-left vert-center">
+			<h4>
+				***REMOVED***<br>
+				Grand Junction, CO 81501
+			</h4>
+		</div>
+		<div class="page-item vert-center">
+			<a href="https://www.focus.org" target="_blank">
+				<img class="contain no-lightbox" src="/2023/10/images/focus-logo.png" alt="FOCUS logo">
+			</a>
+		</div>
+		<div class="page-item double text-right vert-center">
+			<h4>
+				<a href="tel:+16196558686" class="no-decor">(619) 655-8686</a><br>
+				<a href="mailto:edward.hofmeister@focus.org" class="no-decor">edward.hofmeister@focus.org</a>
+			</h4>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE 4 -->
+<div class="page _40">
+	<!-- Header -->
+	<div class="page-section _20">
+		<div class="page-item center-center">
+			<h3>FOCUS National News</h3>
+			<p class="text-large">It is so amazing to see the impact SEEK had on our students and so many others!</p>
+		</div>
+	</div>
+	<!-- FOCUS National News -->
+	<div class="page-section _80">
+		<div class="page-item">
+			<a href="https://seekreplay.com" target="_blank"><img class="photo-item no-lightbox h100" src="/2024/03/images/national-news.png" alt=""></a>
+		</div>
+	</div>
+</div>
