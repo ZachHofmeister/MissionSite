fe01b391dfa953f12e74fe8a04cf489a9183ff67
ZachHofmeister <zachhof@me.com>
2024-04-17 18:52:19 -0600

    finished Mar24 newsletter

--- 2024/03/pages.php
+++ 2024/03/pages.php
@@ -12,14 +12,14 @@
 			<h4 class="italic">Zach Hofmeister<span class="no-style"> | </span>Colorado Mesa University<span class="no-style"> | </span>March 2024</h4>
 		</div>
 		<div class="page-item vert-center">
-			<img class="contain no-lightbox" src="/2023/10/images/cmu-logo.png" alt="CMU logo">
+			<img class="contain no-lightbox" src="/2024/03/images/cmu-logo.png" alt="CMU logo">
 		</div>
 	</div>
 
 	<!-- MAIN IMAGE -->
 	<div class="page-section _30">
 		<div class="page-item">
-			<img class="h100" src="/2024/03/images/upperroom.jpeg" alt="" style="object-position: 50% 45%;" data-caption="">
+			<img class="h100" src="/2024/03/images/peru-group.jpeg" alt="" style="object-position: 50% 45%;" data-caption="Our whole group on our last day in Pamplona and Peru!">
 		</div>
 	</div>
 
@@ -29,26 +29,26 @@
 			<div class="box">
 				<h2 class="italic">Dear Friends,</h2>
 				<p class="text-large">
-					Text
+					He is risen! I hope you're having a blessed Easter! Much of March was spent preparing for and going on our mission trip to Peru, and I am excited to share all about how the Lord was working there and drawing us deeper into His love!
 				</p>
 			</div>
 		</div>
 		<div class="page-item">
-			<img class="photo-item h80" src="/2024/03/images/seek-party.jpeg" alt="" style="object-position: 50% 50%">
-			<p class="caption text-large">Caption</p>
+			<img class="photo-item h80" src="/2024/03/images/peru-above.jpeg" alt="" style="object-position: 50% 50%">
+			<p class="caption text-large">The town square of Pamplona where we played and met our neighbors</p>
 		</div>
 	</div>
 
-	<!-- Upper Room -->
+	<!-- Peru -->
 	<div class="page-section _30">
 		<div class="page-item">
-			<img class="h80" src="/2024/03/images/hot-takes.jpeg" alt="" >
-			<p class="caption text-large">Caption</p>
+			<img class="h80" src="/2024/03/images/peru-jumprope.jpeg" alt="" >
+			<p class="caption text-large">Kylee and Fr. David jumping rope with some of the kids</p>
 		</div>
 		<div class="page-item">
-			<h3>Upper Room</h3>
+			<h3>Pamplona Alta, Peru</h3>
 			<p class="text-large">
-				My favorite highlight this month was our first ever Upper Room at Mesa! Modeled after Jesus gathering with the Apostles, at Upper Room we gather all our disciples together and form them in how to be more intentionally on mission on campus (see top image). 
+				The people we served in Peru live in Pamplona Alta, a mountainous neighborhood on the outskirts of Lima. Our neighbors there were mostly immigrants seeking a better future for their families, even though their living conditions were heartbreaking.
 			</p>
 		</div>
 	</div>
@@ -56,77 +56,68 @@
 
 <!-- PAGE 2 -->
 <div class="page">
-	<!-- Valentine -->
-	<div class="page-section _33">
+	<!-- Peru continued -->
+	<div class="page-section _70">
 		<div class="page-item">
-			<img class="photo-item h100" src="/2024/03/images/early-valentine.jpeg" alt="" >
-		</div>
-		<div class="page-item">
-			<h3>(Early) St. Valentine's Day</h3>
 			<p class="text-large">
-				Since Valentine's Day overlapped with Ash Wednesday this year, we celebrated it early by giving out roses with mass info for the next day! The students were really grateful for the roses, and people started coming to us because of their friends telling them too!
+				We worked mixing concrete to build a retaining wall to protect homes in case of an earthquake. After our work we got to spend time with the people there and play with the kids.
 			</p>
+			<p class="text-large">
+				Each day we also got to accompany our students in daily mass and praying a daily holy hour. This was something new to many of them, and we got to hear some of the awesome and small ways they were encountering the Lord and receiving the grace and strength they needed to continue when being on mission was hard. I certainly needed it! We also got to bring prayer to our neighbors in Pamplona, celebrating mass there our first Sunday and having adoration during our work one day. It was beautiful to join with them in the celebration of the faith!
+			</p>
+		</div>
+		<div class="page-item photo-stack">
+			<div class="photo-item">
+				<img class="h80" src="/2024/03/images/peru-adoration.jpeg" alt="" >
+				<p class="caption text-large">It was such a blessing to adore Jesus in the Eucharist amidst our work</p>
+			</div>
+			<div class="photo-item">
+				<img class="h80" src="/2024/03/images/peru-justin.jpeg" alt="">
+				<p class="caption text-large">Me and Justin, a young adult I met and had a conversation with one night</p>
+			</div>
 		</div>
 	</div>
 
-	<!-- Ash Wednesday -->
-	<div class="page-section _33">
-		<div class="page-item text-right">
-			<h3>Ash Wednesday</h3>
-			<p class="text-large">
-				On Ash Wednesday we had two masses on campus, and manned a table all day to tell people about them. We answered a lot of people on campus confused about why we were calling Valentine's day Ash Wednesday or wondering what it was about, and we had about 80 students total come to mass!
-			</p>
+	<!-- Photos -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/03/images/peru-work.jpeg" alt="">
+			<p class="caption text-large">Our worksite shovel-mixing and delivering concrete</p>
 		</div>
 		<div class="page-item">
-			<img class="photo-item h100" src="/2024/03/images/ash-mass.jpeg" alt="" >
-		</div>
-	</div>
-
-	<!-- Men's Night -->
-	<div class="page-section _33">
-		<div class="page-item">
-			<img class="photo-item h100" src="/2024/03/images/pit-stop.jpeg" alt="" data-caption="The bolts on one of the brakes had mysteriously fallen off - it is a miracle that she drove it to us without losing brake pressure or causing any damage to the car!">
-		</div>
-		<div class="page-item">
-			<h3>Men's Night</h3>
-			<p class="text-large">
-				We've had two of our biweekly men's nights so far, and they've been very successful in uniting the men in our community. We start the nights with confession and prayer, and then have a fun activity! This past week, Nadine had car trouble and we banded together to help, which was a fun surprise!
-			</p>
+			<img class="photo-item h80" src="/2024/03/images/peru-piggyback.jpeg" alt="">
+			<p class="caption text-large">The children loved getting piggyback rides from us</p>
 		</div>
 	</div>
 </div>
 
 <!-- PAGE 3 -->
 <div class="page">
-	<!-- Retreat -->
+	<!-- Pilgrimage -->
 	<div class="page-section _30">
 		<div class="page-item text-right">
-			<h3>Awakening Retreat</h3>
+			<h3>Pilgrimage Day in Lima</h3>
 			<p class="text-large">
-				At the end of the month we went on a weekend retreat led by our student leaders. They did a good job giving talks and sharing their testimonies, and Fr. Carl, a young priest, came to give us Mass, Adoration, and confession! It was great that most of my Bible study came!
+				We took one day to visit where St. Rose of Lima, St. Martin de Porres, St. John Macias, and other holy men and women lived and poured their lives out for Jesus and the poor. It was very inspiring to learn more about their discipleship and continue in their work in a little way.
 			</p>
 		</div>
 		<div class="page-item">
-			<img class="photo-item h80" src="/2024/03/images/retreat-bstud.jpeg" alt="" data-caption="">
-			<p class="caption text-large">Caption</p>
+			<img class="photo-item h80" src="/2024/03/images/peru-convent.jpeg" alt="" data-caption="">
+			<p class="caption text-large">The convent where St. Martin de Porres lived, prayed, and worked</p>
 		</div>
 	</div>
 
 	<!-- Other highlights -->
 	<div class="page-section _30">
 		<div class="page-item">
-			<img class="photo-item h80" src="/2024/03/images/retreat-prayer.jpeg" alt="" style="object-position: 50% 30%;">
-			<p class="caption text-large">Caption</p>
+			<img class="photo-item h80" src="/2024/03/images/peru-basilica.jpeg" alt="" style="object-position: 50% 50%;">
+			<p class="caption text-large">The basilica attached to the convent from the previous picture</p>
 		</div>
 		<div class="page-item">
-			<h3>Other Highlights</h3>
-			<ul class="text-large">
-				<li>Seeing real Christian brotherhood forming in my Bible study</li>
-				<li>Also, playing volleyball against them</li>
-				<li>Inviting Barrett into discipleship!</li>
-				<li>Celebrating Brian's birthday</li>
-				<li>Getting to present about St. Valentine's day and what love really is</li>
-			</ul>
+			<h3>Holy Week</h3>
+			<p class="text-large">
+				Saying goodbye to everyone we met on mission was hard, but it was very good to come back to the rest of our students for Holy Week and Easter! We had a lot of students attend Holy Thursday, Good Friday, and the Easter Vigil with us - but I will wait to tell you all about it in the next newsletter!
+			</p>
 		</div>
 	</div>
 
@@ -135,19 +126,18 @@
 		<div class="page-item">
 			<h2>Please Pray...</h2>
 			<ul class="text-large">
-				<li>For our mission trip to Peru, March 14th-24th! Pray for Jacky, Nadine, Kylee, John, Tessa, Nick, myself and the rest of the students</li>
-				<li>For our disciples to persevere in daily prayer</li>
-				<li>For Christian, Santiago, and Minhtam who I'm inviting to Bible study</li>
-				<li>For Ace's friend Andy who passed away recently</li>
+				<li>For all the families and people living in Pamplona Alta, especially Justin and Alejandro</li>
+				<li>For the students I am inviting to lead their own Bible studies next year</li>
+				<li>For the joy of the resurrection to overflow into a greater desire to share Jesus with others</li>
 			</ul>
 		</div>
 		<div class="page-item text-large">
 			<div class="box">
 				<h2 class="italic">THANK YOU!</h2>
 				<p class="text-large">
-					Thank you for supporting me in this mission through your prayers and generous giving! It is such a blessing to be part of the work Jesus is doing on our campus alongside you!
+					Thank you so much for making it possible for us to accompany our students on this trip! I am praying for you and your families to be renewed in this Easter season!
 				</p>
-				<p class="text-large text-right">All Glory to Jesus, Zach</p>
+				<p class="text-large text-right">In His Resurrection, Zach</p>
 			</div>
 		</div>
 	</div>
@@ -162,7 +152,7 @@
 		</div>
 		<div class="page-item vert-center">
 			<a href="https://www.focus.org" target="_blank">
-				<img class="contain no-lightbox" src="/2023/10/images/focus-logo.png" alt="FOCUS logo">
+				<img class="contain no-lightbox" src="/2024/03/images/focus-logo.png" alt="FOCUS logo">
 			</a>
 		</div>
 		<div class="page-item double text-right vert-center">
@@ -175,18 +165,60 @@
 </div>
 
 <!-- PAGE 4 -->
+<div class="page">
+	<div class="page-section _5">
+		<div class="page-item center-center">
+			<h2>More Photos!</h2>
+		</div>
+	</div>
+	<!-- photos -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/03/images/peru-sign-group.jpeg" alt="">
+			<p class="caption text-large">Our group was made of students from Colorado, Wisconsin, and New York</p>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/03/images/peru-lighthouse.jpeg" alt="">
+			<p class="caption text-large">Our group from CMU: Nadine, myself, Jacky, and Kylee</p>
+		</div>
+	</div>
+	<!-- photos -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/03/images/peru-rose.jpeg" alt="">
+			<p class="caption text-large">Visiting the chapel where St. Rose of Lima is buried</p>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/03/images/peru-leaving.jpeg" alt="">
+			<p class="caption text-large">Our last hike down the steep mountain of Pamplona to go home</p>
+		</div>
+	</div>
+	<!-- photos -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/03/images/upper-room.jpeg" alt="">
+			<p class="caption text-large">Our monthly upper room meetings with our disciples have been growing and sparking great discussions</p>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/03/images/axe-throwing.jpeg" alt="">
+			<p class="caption text-large">We went axe throwing with some of the guys for men's night</p>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE 5 -->
 <div class="page _40">
 	<!-- Header -->
 	<div class="page-section _20">
 		<div class="page-item center-center">
 			<h3>FOCUS National News</h3>
-			<p class="text-large">It is so amazing to see the impact SEEK had on our students and so many others!</p>
+			<p class="text-large">Take a look at this short reflection from one of our national chaplains!</p>
 		</div>
 	</div>
 	<!-- FOCUS National News -->
 	<div class="page-section _80">
 		<div class="page-item">
-			<a href="https://seekreplay.com" target="_blank"><img class="photo-item no-lightbox h100" src="/2024/03/images/national-news.png" alt=""></a>
+			<img class="photo-item h100" src="/2024/03/images/national-news.png" alt="">
 		</div>
 	</div>
 </div>
