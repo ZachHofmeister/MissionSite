1fbe4032a09a8b4fe82232298828e8dc0dfccc2f
ZachHofmeister <zachhof@me.com>
2024-05-18 22:49:29 -0600

    first draft April-May newsletter

--- 2024/04/pages.php
+++ 2024/04/pages.php
@@ -9,7 +9,7 @@
 		</div>
 		<div class="page-item vert-center text-center w6">
 			<h1 class="cardo">The Mission at Mesa</h1>
-			<h4 class="italic">Zach Hofmeister<span class="no-style"> | </span>Colorado Mesa University<span class="no-style"> | </span>April 2024</h4>
+			<h5 class="italic">Zach Hofmeister<span class="no-style"> | </span>Colorado Mesa University<span class="no-style"> | </span>April-May 2024</h5>
 		</div>
 		<div class="page-item vert-center">
 			<img class="contain no-lightbox" src="/2024/04/images/cmu-logo.png" alt="CMU logo">
@@ -19,104 +19,171 @@
 	<!-- MAIN IMAGE -->
 	<div class="page-section _30">
 		<div class="page-item">
-			<img class="h100" src="/2024/04/images/peru-group.jpeg" alt="" style="object-position: 50% 45%;" data-caption="Our whole group on our last day in Pamplona and Peru!">
+			<img class="h100" src="/2024/04/images/easter-vigil.jpeg" alt="" style="object-position: 50% 50%;" data-caption="Our students who came to the Easter Vigil mass with us!">
 		</div>
 	</div>
 
 	<!-- INTRO / PHOTO -->
 	<div class="page-section _30">
-		<div class="page-item text-right">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/04/images/easter-hike.jpeg" alt="" style="object-position: 50% 50%">
+			<p class="caption text-large">We invited our students to go on a hike Saturday afternoon before Easter Vigil</p>
+		</div>
+		<div class="page-item">
 			<div class="box">
 				<h2 class="italic">Dear Friends,</h2>
 				<p class="text-large">
-					He is risen! I hope you're having a blessed Easter! Much of March was spent preparing for and going on our mission trip to Peru, and I am excited to share all about how the Lord was working there and drawing us deeper into His love!
+					Happy Easter! I pray that you are doing well. I am excited to share about how God has been working in our lives these last two months of the semester, and to also share about my hopes for this next year of mission at CMU!
 				</p>
 			</div>
 		</div>
-		<div class="page-item">
-			<img class="photo-item h80" src="/2024/04/images/peru-above.jpeg" alt="" style="object-position: 50% 50%">
-			<p class="caption text-large">The town square of Pamplona where we played and met our neighbors</p>
-		</div>
 	</div>
 
-	<!-- Peru -->
+	<!-- photo-text -->
 	<div class="page-section _30">
-		<div class="page-item">
-			<img class="h80" src="/2024/04/images/peru-jumprope.jpeg" alt="" >
-			<p class="caption text-large">Kylee and Fr. David jumping rope with some of the kids</p>
+		<div class="page-item text-right">
+			<h3>Easter Joy</h3>
+			<p class="text-large">
+				It was so good to be with everyone for Holy Week and Easter after returning from Peru! A lot of our students joined us for the Easter Vigil Mass. To celebrate, we had a big brunch at the girls' house and took our joy to campus and Newman all that week too.
+			</p>
 		</div>
 		<div class="page-item">
-			<h3>Pamplona Alta, Peru</h3>
-			<p class="text-large">
-				The people we served in Peru live in Pamplona Alta, a mountainous neighborhood on the outskirts of Lima. Our neighbors there were mostly immigrants seeking a better future for their families, even though their living conditions were heartbreaking.
-			</p>
+			<img class="h80" src="/2024/04/images/easter-brunch.jpeg" alt="" >
+			<p class="caption text-large">Some of us playing hackysack at our Easter brunch</p>
 		</div>
 	</div>
 </div>
 
 <!-- PAGE 2 -->
 <div class="page">
-	<!-- Peru continued -->
-	<div class="page-section _70">
-		<div class="page-item">
-			<p class="text-large">
-				We worked mixing concrete to build a retaining wall to protect homes in case of an earthquake. After our work we got to spend time with the people there and play with the kids.
-			</p>
-			<p class="text-large">
-				Each day we also got to accompany our students in daily mass and praying a daily holy hour. This was something new to many of them, and we got to hear some of the awesome and small ways they were encountering the Lord and receiving the grace and strength they needed to continue when being on mission was hard. I certainly needed it! We also got to bring prayer to our neighbors in Pamplona, celebrating mass there our first Sunday and having adoration during our work one day. It was beautiful to join with them in the celebration of the faith!
-			</p>
-		</div>
-		<div class="page-item photo-stack">
-			<div class="photo-item">
-				<img class="h80" src="/2024/04/images/peru-adoration.jpeg" alt="" >
-				<p class="caption text-large">It was such a blessing to adore Jesus in the Eucharist amidst our work</p>
-			</div>
-			<div class="photo-item">
-				<img class="h80" src="/2024/04/images/peru-justin.jpeg" alt="">
-				<p class="caption text-large">Me and Justin, a young adult I met and had a conversation with one night</p>
-			</div>
+	<!-- header -->
+	<div class="page-section _10">
+		<div class="page-item center-center">
+			<h2>From Easter Joy to Mission</h2>
+			<p class="text-large">“Peace be with you. As the Father has sent me, even so I send you.” - John 20:21</p>
 		</div>
 	</div>
 
-	<!-- Photos -->
+	<!-- photo-text -->
 	<div class="page-section _30">
 		<div class="page-item">
-			<img class="photo-item h80" src="/2024/04/images/peru-work.jpeg" alt="">
-			<p class="caption text-large">Our worksite shovel-mixing and delivering concrete</p>
+			<img class="h80" src="/2024/04/images/election-bros.jpeg" alt="" >
+			<p class="caption text-large">Drew, Gabe, Elijah, Brenden, and Robert on club election night</p>
 		</div>
 		<div class="page-item">
-			<img class="photo-item h80" src="/2024/04/images/peru-piggyback.jpeg" alt="">
-			<p class="caption text-large">The children loved getting piggyback rides from us</p>
+			<h3>New Club Leadership</h3>
+			<p class="text-large">
+				A few of my freshmen guys ran for positions at our student elections! Drew is our House Coordinator and Robert is head of Friday fun events for next year. These are important responsibilities for outreach to students, so it is awesome to see them take them on!
+			</p>
+		</div>
+	</div>
+
+	<!-- photo-text -->
+	<div class="page-section _30">
+		<div class="page-item text-right">
+			<h3>Warm Weather Outreach</h3>
+			<p class="text-large">
+				We can finally start bringing outdoor games like Spikeball and frisbee to campus for our outreach again! We've met some awesome new people like Jason and Payton, and it has been a great way to model the Little Way of Evangelization to our students too.
+			</p>
+		</div>
+		<div class="page-item">
+			<img class="h80" src="/2024/04/images/spike-outreach.jpeg" alt="" >
+			<p class="caption text-large">Playing Spikeball on campus for outreach</p>
+		</div>
+	</div>
+	
+	<!-- photo-text -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h80" src="/2024/04/images/drew-firefighter.jpeg" alt="" >
+			<p class="caption text-large">Drew and I got to try out a firehose walking and talking around campus</p>
+		</div>
+		<div class="page-item">
+			<h3>Discipleship Updates</h3>
+			<p class="text-large">
+				All of the guys in my Bible study are in discipleship now, since I invited Brenden at the end of April! Furthermore, I'll be walking with 3 of them in starting to lead their own Bible studies next semester. I am so excited to see what God will do through their yes!
+			</p>
 		</div>
 	</div>
 </div>
 
 <!-- PAGE 3 -->
 <div class="page">
-	<!-- Pilgrimage -->
+	<!-- header -->
+	<div class="page-section _10">
+		<div class="page-item center-center">
+			<h2>Looking to the Future</h2>
+			<p class="text-large">“I will make a way in the wilderness and rivers in the desert.” - Isaiah 43:19</p>
+		</div>
+	</div>
+
+	<!-- photo-text -->
 	<div class="page-section _30">
 		<div class="page-item text-right">
-			<h3>Pilgrimage Day in Lima</h3>
+			<h3>Next Year at Mesa</h3>
 			<p class="text-large">
-				We took one day to visit where St. Rose of Lima, St. Martin de Porres, St. John Macias, and other holy men and women lived and poured their lives out for Jesus and the poor. It was very inspiring to learn more about their discipleship and continue in their work in a little way.
+				I'm happy to share that I will be returning to CMU next school year for my third year on mission! I am excited to continue building relationships with students I've met this year, and to walk with them as they begin to be more fully on mission to their peers!
 			</p>
 		</div>
 		<div class="page-item">
-			<img class="photo-item h80" src="/2024/04/images/peru-convent.jpeg" alt="" data-caption="">
-			<p class="caption text-large">The convent where St. Martin de Porres lived, prayed, and worked</p>
+			<img class="h90" src="/2024/04/images/fr-paul.jpeg" alt="" >
+			<p class="caption text-large">Our team with Fr. Paul, our chaplain</p>
+		</div>
+	</div>
+	
+	<!-- photo-text -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h90" src="/2024/04/images/melvin-brian.jpeg" alt="" style="object-position: 50% 25%">
+			<p class="caption text-large">Brian and Melvin hanging out</p>
+		</div>
+		<div class="page-item">
+			<!-- <h3>Old Friends and New</h3> -->
+			<p class="text-large">
+				My teammate Jacky will be staying here next year too, but we will sadly be saying goodbye to Brian and Ally. Our new teammates next year will be my friend Joseph, who I went to school with, and Claire, who is friends with Jacky! Please keep us all in your prayers as we make this transition!
+			</p>
+		</div>
+	</div>
+
+	<!-- photo-text -->
+	<div class="page-section _30">
+		<div class="page-item text-right">
+			<h3>Meet Allison</h3>
+			<p class="text-large">
+				I thought it would also be a good time to share that for the past 6 months Allison and I have been dating! She lives with my female teammates and teaches math at a nearby high school. Please keep our relationship in your prayers, and God willing you'll see her more!
+			</p>
+		</div>
+		<div class="page-item">
+			<img class="h90" src="/2024/04/images/allison.jpeg" alt="" >
+			<p class="caption text-large">Allison and I at the Easter Vigil Mass</p>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE 4 -->
+<div class="page">
+	<!-- photos -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/04/images/volley-team.jpeg" alt="" data-caption="">
+			<p class="caption text-large">A lot of my Bible study guys played intramural volleyball this semester</p>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/04/images/shooting-range.jpeg" alt="" data-caption="">
+			<p class="caption text-large">We took a men's day trip to the shooting range</p>
 		</div>
 	</div>
 
 	<!-- Other highlights -->
 	<div class="page-section _30">
 		<div class="page-item">
-			<img class="photo-item h80" src="/2024/04/images/peru-basilica.jpeg" alt="" style="object-position: 50% 50%;">
-			<p class="caption text-large">The basilica attached to the convent from the previous picture</p>
+			<img class="photo-item h80" src="/2024/04/images/melvin.jpeg" alt="" style="object-position: 50% 50%;">
+			<p class="caption text-large">Melvin will be taken care of over the summer by our friend Keegan</p>
 		</div>
 		<div class="page-item">
-			<h3>Holy Week</h3>
+			<h3>Summer Time 😎☀️</h3>
 			<p class="text-large">
-				Saying goodbye to everyone we met on mission was hard, but it was very good to come back to the rest of our students for Holy Week and Easter! We had a lot of students attend Holy Thursday, Good Friday, and the Easter Vigil with us - but I will wait to tell you all about it in the next newsletter!
+				This summer I will be helping at training in Lincoln, NE as a member of the Liturgy Committee! I will also be keeping in touch with my disciples to continue growing in their faith over their break. I would love the chance to meet with <b><u>you</u></b> in person also!
 			</p>
 		</div>
 	</div>
@@ -126,18 +193,18 @@
 		<div class="page-item">
 			<h2>Please Pray...</h2>
 			<ul class="text-large">
-				<li>For all the families and people living in Pamplona Alta, especially Justin and Alejandro</li>
-				<li>For the students I am inviting to lead their own Bible studies next year</li>
-				<li>For the joy of the resurrection to overflow into a greater desire to share Jesus with others</li>
+				<li>For all the disciples at CMU Mesa, especially for the Holy Spirit to fill them and guide them over summer</li>
+				<li>For our current team: Jacky, Ally, and Brian, and our new teammates Joe and Claire</li>
+				<li>For the students who God wants to reach next year and beyond through our work of evangelization, and for our perseverance</li>
 			</ul>
 		</div>
 		<div class="page-item text-large">
 			<div class="box">
 				<h2 class="italic">THANK YOU!</h2>
 				<p class="text-large">
-					Thank you so much for making it possible for us to accompany our students on this trip! I am praying for you and your families to be renewed in this Easter season!
+					I am so grateful for your prayerfulness and generosity. Thank you so much for being my partner in this mission! I cannot wait to journey forward with you into summer and this next year!
 				</p>
-				<p class="text-large text-right">In His Resurrection, Zach</p>
+				<p class="text-large text-right">In The Spirit of God, Zach</p>
 			</div>
 		</div>
 	</div>
@@ -174,51 +241,33 @@
 	<!-- photos -->
 	<div class="page-section _30">
 		<div class="page-item">
-			<img class="photo-item h80" src="/2024/04/images/peru-sign-group.jpeg" alt="">
-			<p class="caption text-large">Our group was made of students from Colorado, Wisconsin, and New York</p>
+			<img class="photo-item h90" src="/2024/04/images/election.jpeg" alt="">
+			<p class="caption text-large">More people from election night</p>
 		</div>
 		<div class="page-item">
-			<img class="photo-item h80" src="/2024/04/images/peru-lighthouse.jpeg" alt="">
-			<p class="caption text-large">Our group from CMU: Nadine, myself, Jacky, and Kylee</p>
+			<img class="photo-item h90" src="/2024/04/images/rodeo.jpeg" alt="">
+			<p class="caption text-large">Watching the CMU rodeo</p>
 		</div>
 	</div>
 	<!-- photos -->
 	<div class="page-section _30">
 		<div class="page-item">
-			<img class="photo-item h80" src="/2024/04/images/peru-rose.jpeg" alt="">
-			<p class="caption text-large">Visiting the chapel where St. Rose of Lima is buried</p>
+			<img class="photo-item h90" src="/2024/04/images/unmatched.jpeg" alt="">
+			<p class="caption text-large">Men's board game night</p>
 		</div>
 		<div class="page-item">
-			<img class="photo-item h80" src="/2024/04/images/peru-leaving.jpeg" alt="">
-			<p class="caption text-large">Our last hike down the steep mountain of Pamplona to go home</p>
+			<img class="photo-item h90" src="/2024/04/images/volley.jpeg" alt="">
+			<p class="caption text-large">The freshmen boys' volleyball match</p>
 		</div>
 	</div>
 	<!-- photos -->
 	<div class="page-section _30">
 		<div class="page-item">
-			<img class="photo-item h80" src="/2024/04/images/upper-room.jpeg" alt="">
-			<p class="caption text-large">Our monthly upper room meetings with our disciples have been growing and sparking great discussions</p>
+			<img class="photo-item h90" src="/2024/04/images/spike-easter.jpeg" alt="">
+			<p class="caption text-large">Easter brunch playing Spikeball</p>
 		</div>
 		<div class="page-item">
-			<img class="photo-item h80" src="/2024/04/images/axe-throwing.jpeg" alt="">
-			<p class="caption text-large">We went axe throwing with some of the guys for men's night</p>
-		</div>
-	</div>
-</div>
-
-<!-- PAGE 5 -->
-<div class="page _40">
-	<!-- Header -->
-	<div class="page-section _20">
-		<div class="page-item center-center">
-			<h3>FOCUS National News</h3>
-			<p class="text-large">Take a look at this short reflection from one of our national chaplains!</p>
-		</div>
-	</div>
-	<!-- FOCUS National News -->
-	<div class="page-section _80">
-		<div class="page-item">
-			<img class="photo-item h100" src="/2024/04/images/national-news.png" alt="">
+			<img class="photo-item h90" src="/2024/04/images/spike-win.jpeg" alt=""><p class="caption text-large">Gabe and Robert won 2nd in a Spikeball tournament on campus!</p>
 		</div>
 	</div>
 </div>
