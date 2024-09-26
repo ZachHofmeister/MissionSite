e4bf5f76ecb22b25bfba4d2502d93b8327c618b5
ZachHofmeister <zachhof@me.com>
2024-09-26 09:51:10 -0600

    finished Aug24 Newsletter

---
+++ 2024/08/pages.php
@@ -0,0 +1,143 @@
+<!-- Yours, O Lord, are the greatness, the power, the glory, the victory, and the majesty; for all that is in the heavens and on the earth is yours; yours is the kingdom, O Lord, and you are exalted as head above all. - 1 Chronicles 29:11 -->
+
+<!-- PAGE 1 -->
+<div class="page">
+	<!-- HEADER -->
+	<div class="page-section _10">
+		<div class="page-item vert-center">
+			<img class="contain no-lightbox" src="/2024/08/images/focus-logo.png" alt="FOCUS logo">
+		</div>
+		<div class="page-item vert-center text-center w4 nopad">
+			<h1 class="cardo">The Mission at Mesa</h1>
+			<h5 class="italic" style="white-space: nowrap;">Zach Hofmeister<span class="no-style"> | </span>Colorado Mesa University<span class="no-style"> | </span>August 2024</h5>
+		</div>
+		<div class="page-item vert-center">
+			<img class="contain no-lightbox" src="/2024/08/images/cmu-logo.png" alt="CMU logo">
+		</div>
+	</div>
+
+	<!-- MAIN IMAGE -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h100" src="/2024/08/images/smores.jpeg" alt="" style="object-position: 50% 50%;" data-caption="We had a great turnout for s'mores night during the first week of school, including Father Carl, an awesome young priest who recently was moved to Grand Junction!">
+		</div>
+	</div>
+
+	<!-- INTRO / PHOTO -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<div class="box">
+				<h2 class="italic">Dear Friends,</h2>
+				<p class="text-large">
+					I hope you are doing well! It has been a while since my last update, but I am excited to share with you how our mission at Mesa has been starting out. Here are some of the highlights from August!
+				</p>
+			</div>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/08/images/banana.jpeg" alt="" style="object-position: 50% 50%" data-caption="! Clare dressed up as a banana and we asked people questions in exchange for the tropical fruit.">
+			<p class="caption text-large">We met lots of people with banana-themed outreach, among other fun ideas</p>
+		</div>
+	</div>
+
+	<!-- Paragraph + Image -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h80" src="/2024/08/images/hike.jpeg" alt="" >
+			<p class="caption text-large">Me, Drew, and Nick on a hike in the Colorado National Monument!</p>
+		</div>
+		<div class="page-item">
+			<h3>Old Friends</h3>
+			<p class="text-large">
+				It was great to reconnect with our students when they came back from break! We had fun introducing them to our new teammates, Joe and Clare, hearing about their experiences and faithfulness over the summer break, and doing fun activities together like hiking.
+			</p>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE 3 -->
+<div class="page _50">
+	<div class="page-section _15">
+		<div class="page-item center-center">
+			<h2>Meet our new team!</h>
+		</div>
+	</div>
+	<div class="page-section _85">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/08/images/team-mesa.jpeg" alt="" data-caption="">
+			<p class="caption text-large">From left to right: Joe, Clare, me, and Jacky! Joe and I graduated together from Cal State Fullerton, and Jacky and Clare were friends before this year too! We've all been working well together and bringing our own gifts to the team.</p>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE 3 -->
+<div class="page">
+	<!-- photos -->
+	<div class="page-section _25">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/08/images/mens-night.jpeg" alt="" data-caption="">
+			<p class="caption text-large">Men's night with all of the men in discipleship</p>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/08/images/bww.jpeg" alt="" data-caption="">
+			<p class="caption text-large">Father Carl joined us for a guys' night after mass - he is awesome!</p>
+		</div>
+	</div>
+
+	<!-- Other highlights -->
+	<div class="page-section _25">
+		<div class="page-item">
+			<h3>New Friends</h3>
+			<p class="text-large">
+				The first 2 weeks of school we were busy meeting new students on campus and inviting then to Mesa Catholic. The chapel for our first two Sunday Masses was overflowing - a really good problem to have! Praise God!
+			</p>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h90" src="/2024/08/images/mass.jpeg" alt="" style="object-position: 50% 50%;">
+			<p class="caption text-large">Our group for mass the first week of school</p>
+		</div>
+	</div>
+
+	<div class="page-section _15">
+		<div class="page-item">
+			<h3>Prayer intentions</h3>
+			<p class="text-large">Please pray for Keegan who is recovering from a motorcycle accident concussion, for all of our students we are inviting to SEEK, and for an increase in courage to share the faith with those who need it most!</p>
+		</div>
+	</div>
+
+	<!-- PRAYER REQUESTS / THANK YOU -->
+	<div class="page-section _25">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2024/08/images/palisade-hike.jpeg" alt="" style="object-position: 50% 50%;">
+			<p class="caption text-large">Me, Allison, Joe, and Clare went on a hike together before school started</p>
+		</div>
+		<div class="page-item text-large">
+			<div class="box">
+				<h2 class="italic">THANK YOU!</h2>
+				<p class="text-large">
+					Thank you so much for being a partner in my mission at Colorado Mesa. It is such a blessing to share in God's work here with you!
+				</p>
+				<p class="text-large text-right">
+					In Christ, Zach
+				</p>
+			</div>
+		</div>
+	</div>
+	
+	<!-- FOOTER -->
+	<div class="page-section _10">
+		<div class="page-item w2 text-right vert-center" style="padding: 0;">
+			<h4 style="padding: 0 2%;">
+				<a href="mailto:edward.hofmeister@focus.org" class="no-decor">edward.hofmeister@focus.org</a><br>
+				<a href="tel:+16196558686" class="no-decor">(619) 655-8686</a>
+			</h4>
+		</div>
+		<div class="page-item text-left vert-center" style="padding: 0;">
+			<a href="https://www.focus.org" target="_blank" style="padding: 0 4%;">
+				<img class="contain no-lightbox" src="/2024/08/images/focus-logo.png" alt="FOCUS logo">
+			</a>
+		</div>
+		<div class="page-item text-left vert-center" style="padding: 0;">
+			
+		</div>
+	</div>
+</div>
--- config.php
+++ config.php
@@ -1,41 +1,40 @@
 <?php 
-	error_reporting(E_ALL); // Error/Exception engine, always use E_ALL
+error_reporting(E_ALL); // Error/Exception engine, always use E_ALL
 
-	ini_set('ignore_repeated_errors', TRUE); // always use TRUE
-	
-	ini_set('display_errors', FALSE); // Error/Exception display, use FALSE only in production environment or real server. Use TRUE in development environment
-	
-	ini_set('log_errors', TRUE); // Error/Exception file logging engine.
-	ini_set('error_log', '/phpErrors/phpErrorLog-missionSite.log'); // Logging file path
+ini_set('ignore_repeated_errors', TRUE); // always use TRUE
 
-	session_start();
+ini_set('display_errors', FALSE); // Error/Exception display, use FALSE only in production environment or real server. Use TRUE in development environment
 
-	define ('ROOT_PATH', realpath(dirname(__FILE__)));
-	// define('BASE_URL', 'http://localhost/');
+ini_set('log_errors', TRUE); // Error/Exception file logging engine.
+ini_set('error_log', '/phpErrors/phpErrorLog-missionSite.log'); // Logging file path
 
-	$pathsJson = file_get_contents(ROOT_PATH . "/.paths.json");
-	$paths = json_decode($pathsJson, true);
+session_start();
 
-	// connect to database
+define ('ROOT_PATH', realpath(dirname(__FILE__)));
+// define('BASE_URL', 'http://localhost/');
 
-	$json = file_get_contents($paths['creds']);
-	$creds = json_decode($json, true);
+$pathsJson = file_get_contents(ROOT_PATH . "/.paths.json");
+$paths = json_decode($pathsJson, true);
 
-	$host = $creds['host'] ?? null;
-	$user = $creds['username'] ?? null;
-	$pass = $creds['password'] ?? null;
-	$db = $creds['schema'] ?? null;
-	$port = $creds['port'] ?? null;
+// connect to database
 
-	$conn = null;
+$json = file_get_contents($paths['creds']);
+$creds = json_decode($json, true);
 
-	try {
-		$conn = mysqli_connect($host, $user, $pass, $db, $port);
-	} catch (Exception $e) {
-		// echo "Exception: " . $e;
-		if (!$conn) {
-			die("Error");
-			// die("Error connecting to database: " . mysqli_connect_error());
-		}
+$host = $creds['host'] ?? null;
+$user = $creds['username'] ?? null;
+$pass = $creds['password'] ?? null;
+$db = $creds['schema'] ?? null;
+$port = $creds['port'] ?? null;
+
+$conn = null;
+
+try {
+	$conn = mysqli_connect($host, $user, $pass, $db, port: $port);
+} catch (Exception $e) {
+	// echo "Exception: " . $e;
+	if (!$conn) {
+		die("Error");
+		// die("Error connecting to database: " . mysqli_connect_error());
 	}
-?>
+}
