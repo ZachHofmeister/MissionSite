799cd6e6f5a225aed9ecabdec6d66facc04ef94b
ZachHofmeister <zachhof@me.com>
2024-09-26 13:29:51 -0600

    fixed config?

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 	error_reporting(E_ALL); // Error/Exception engine, always use E_ALL
 
 	ini_set('ignore_repeated_errors', TRUE); // always use TRUE
-
+	
 	ini_set('display_errors', FALSE); // Error/Exception display, use FALSE only in production environment or real server. Use TRUE in development environment
-
+	
 	ini_set('log_errors', TRUE); // Error/Exception file logging engine.
 	ini_set('error_log', '/phpErrors/phpErrorLog-missionSite.log'); // Logging file path
 
@@ -30,7 +30,7 @@
 	$conn = null;
 
 	try {
-		$conn = mysqli_connect($host, $user, $pass, $db, port: $port);
+		$conn = mysqli_connect($host, $user, $pass, $db, $port);
 	} catch (Exception $e) {
 		// echo "Exception: " . $e;
 		if (!$conn) {
