d77542e4fa50f92acd573da48caec7413b74d7d6
ZachHofmeister <zachhof@me.com>
2024-10-02 12:11:34 -0600

    getting newsletter to populate <title> from DB

--- newsletter.php
+++ newsletter.php
@@ -1,28 +1,20 @@
-<?php
-	//Require config file
-	require_once('config.php');
-	//sets the NEWSLETTER_DIR and NEWSLETTER_DIR_REL constant
-	include(ROOT_PATH . '/includes/get-newsletter-dir.php');
-
-	//Toggle editing button
-	function toggleEditing() {
-		// Store date value
-		$date = $_GET['date'];
-		// Redirect
-		if (isset($_GET["editing"])) {
-			header('location: /newsletter.php?date='.$date);
-		} else {
-			header('location: /newsletter.php?date='.$date.'&editing=1');
-		}
-	}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
+	<?php
+		require_once('config.php');
+		//sets the NEWSLETTER_DIR and NEWSLETTER_DIR_REL constant
+		include(ROOT_PATH . '/includes/get-newsletter-dir.php');
+		// $newsletter = Newsletter::fetchByEdition(NEWSLETTER_EDITION);
+		
+	?>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-		<?php include(NEWSLETTER_DIR . '/head.php');?>
-		<!-- <link rel="stylesheet" type="text/css" href="/css/home.css/"> -->
+		<?php
+			// echo '<link rel="stylesheet" type="text/css" href="' . NEWSLETTER_DIR_REL . '/colors.css">'
+			include(NEWSLETTER_DIR . '/head.php');
+			// echo '<title>September 2024 Update</title>
+			// <link rel="stylesheet" type="text/css" href="/2024/09/colors.css">'
+		?>
 		<link rel="stylesheet" type="text/css" href="/css/newsletter.css">
 		<link rel="stylesheet" type="text/css" href="/css/lightbox.css">
 		<link rel="stylesheet" type="text/css" href="/css/navbar.css">
@@ -45,5 +37,9 @@
 
 		<!-- LIGHTBOX -->
 		<?php include(ROOT_PATH . '/includes/lightbox.php'); ?>
+
+		<!-- JAVASCRIPT -->
+		<script src="/js/navbar.js"></script>
+		<script src="/js/lightbox.js"></script>
 	</body>
 </html>
