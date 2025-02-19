4f27a05fef81fb80b9191c86f3a3aea1c3ae595f
ZachHofmeister <zachhof@me.com>
2025-02-19 11:38:34 -0700

    started feb25 newsletter

---
+++ 2025/02/head.php
@@ -0,0 +1,2 @@
+<title>February 2025 Update</title>
+<link rel="stylesheet" type="text/css" href="/2025/02/colors.css">
---
+++ 2025/02/pages.php
@@ -0,0 +1,201 @@
+<!-- Yours, O Lord, are the greatness, the power, the glory, the victory, and the majesty; for all that is in the heavens and on the earth is yours; yours is the kingdom, O Lord, and you are exalted as head above all. - 1 Chronicles 29:11 -->
+
+<!-- PAGE 1 -->
+<div class="page">
+	<!-- HEADER -->
+	<div class="page-section _10">
+		<div class="page-item vert-center">
+			<img class="contain no-lightbox" src="/2025/02/images/focus.png" alt="FOCUS logo">
+		</div>
+		<div class="page-item vert-center text-center w4 nopad">
+			<h1 class="cardo">The Mission at Mesa</h1>
+			<h5 class="italic" style="white-space: nowrap;">Zach Hofmeister<span class="no-style"> | </span>Colorado Mesa University<span class="no-style"> | </span>February 2025</h5>
+		</div>
+		<div class="page-item vert-center">
+			<img class="contain no-lightbox" src="/2025/02/images/cmu.png" alt="CMU logo">
+		</div>
+	</div>
+
+	<!-- MAIN IMAGE -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h100" src="/2025/02/images/dinner.jpeg" alt="" style="object-position: 50% 60%;" data-caption="Most of our students after the dinner we catered at SEEK!">
+		</div>
+	</div>
+
+	<!-- INTRO / PHOTO -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="photo-item h90" src="/2025/02/images/flag.jpeg" alt="" style="object-position: 50% 50%">
+			<p class="caption text-large">caption</p>
+		</div>
+		<div class="page-item">
+			<div class="box">
+				<h2 class="italic">Dear Friends,</h2>
+				<p class="text-large">
+					It's good to be back! Our students didn't start class 'til the 21st, but a lot has been going on! Over break we had the SEEK conference and a retreat with missionaries and students, and there's lots to share from starting the semester too!
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<!-- Paragraph + Image -->
+	<div class="page-section _30">
+		<div class="page-item text-right">
+			<h3>SEEK Conference</h3>
+			<p class="text-medium">
+				We began our new year in Salt Lake City for SEEK25! In 2020 my life was forever changed when I encountered Jesus' heart anew at a FOCUS conference, and I love to witness Him working in our students there. It was amazing getting to go to powerful talks, daily Mass, and Adoration with them. <a href="https://seekreplay.com/video/141" target="_blank">This</a> was one of my favorite talks!
+			</p>
+		</div>
+		<div class="page-item">
+			<img class="h80" src="/2025/02/images/dudes.jpeg" alt="" >
+			<p class="caption text-large">caption</p>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE 2 -->
+<div class="page">
+	<!-- quote -->
+	<div class="page-section _10">
+		<div class="page-item center-center">
+			<p class="text-large text-center"><i>And a voice came from the heavens, saying, “This is my beloved Son, with whom I am well pleased.”</i> - <a href="https://www.biblegateway.com/passage/?search=Matthew%203%3A16%2D17&version=NABRE" target="_blank">Matthew 3:17</a> (this verse was powerful for me at SEEK and since!)</p>
+		</div>
+	</div>
+
+	<!-- photos -->
+	<div class="page-section _25">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2025/02/images/bananas.jpeg" alt="" >
+			<p class="caption text-large">caption</p>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2025/02/images/group.jpeg" alt="" >
+			<p class="caption text-large">caption</p>
+		</div>
+	</div>
+
+	<!-- Missionary Summit -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2025/02/images/summit.jpeg" alt="" style="object-position: 50% 50%;">
+			<p class="caption text-large">caption</p>
+		</div>
+		<div class="page-item">
+			<h3>Missionary Summit</h3>
+			<p class="text-medium">
+				The weekend before the semester started, FOCUS invited us, our students, and other campuses to a retreat. It was a powerful time to reflect on being God's beloved children, and being fishers of men and going out into the deep to share the Gospel on campus. I got to catch up with some of the Air Force cadets there as well!
+			</p>
+		</div>
+	</div>
+
+	<!-- Exodus 90 -->
+	<div class="page-section _35">
+		<div class="page-item text-right">
+			<h3>Exodus 90</h3>
+			<p class="text-medium">
+				At SEEK, a lot of the men were inspired to start <a href="https://exodus90.com" target="_blank">Exodus 90</a>: a 90-day spiritual exercise of fasting, asceticism, and prayer leading up to Easter. I honestly didn't want to do it, but felt called to go through it with the guys this year - and I'm so glad I did. It has been especially cool to see their increased commitment to prayer and Mass - some of them have even started praying for an hour each day!
+			</p>
+		</div>
+		<div class="page-item">
+			<img class="h80" src="/2025/02/images/exodus.jpeg" alt="" data-caption=". LtR: Dylan, Keegan, Barrett, Tony, John, Jorge, Christian, Elijah, Joe, and Nick.">
+			<p class="caption text-large">caption</p>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE 3 -->
+<div class="page">
+
+	<!-- photo-text -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="photo-item h90" src="/2025/02/images/barrett.jpeg" alt="" style="object-position: 50% 50%;">
+			<p class="caption text-large">caption</p>
+		</div>
+		<div class="page-item">
+			<h3>Bible Studies</h3>
+			<p class="text-medium">
+				We have been praying for more student-led men's Bible studies at Mesa for a long time. Last semester we had our first one ever, and this semester we have 3 more starting! At SEEK, Barrett went to talks on leading a Bible study and came back excited to start his own! I'm so proud of him and grateful to God for this answered prayer.
+			</p>
+		</div>
+	</div>
+
+	<!-- Pics -->
+	<div class="page-section _20">
+		<div class="page-item">
+			<img class="h80" src="/2025/02/images/concert.jpeg" alt="" style="object-position: 50% 50%;" data-caption=". It was a lot of fun to be up at the front of the concert with a priest! Lots of people came up and wanted to know where his parish was.">
+			<p class="caption text-large">caption</p>
+		</div>
+		<div class="page-item">
+			<img class="h80" src="/2025/02/images/robert.jpeg" alt="" style="object-position: 50% 30%;">
+			<p class="caption text-large">caption</p>
+		</div>
+	</div>
+
+	<!-- PRAYER INTENTIONS -->
+	<div class="page-section _15">
+		<div class="page-item">
+			<h3 style="margin-top: 5px;">Prayer Requests</h3>
+			<p class="text-medium">
+				Please pray for our students fundraising for their mission trip, for our student Bible study leaders (Barrett, Matthew, Ryan, Keegan, Olivia, Brenna, Sophie, Kylee, and Bridget) and those they're inviting, and for an increase of chastity, zeal for souls, and fortitude.
+			</p>
+		</div>
+	</div>
+
+	<!-- PHOTO / THANK YOU -->
+	<div class="page-section _25">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2025/02/images/kiln.jpeg" alt="" style="object-position: 50% 50%;">
+			<p class="caption text-large">caption</p>
+		</div>
+		<div class="page-item text-medium">
+			<div class="box">
+				<h2 class="italic">THANK YOU!</h2>
+				<p class="text-medium">Thank you for being on mission with me here at Mesa! Your prayers are powerful with God and I am grateful for your generosity. I am praying for you.</p>
+				<p class="text-medium text-right">
+					In the Father's love, Zach
+				</p>
+			</div>
+		</div>
+	</div>
+	
+	<!-- FOOTER -->
+	<div class="page-section _10">
+		<div class="page-item w2 text-right vert-center" style="padding: 0;">
+			<h4 style="padding: 0 2%;">
+				<a href="mailto:edward.hofmeister@focus.org" class="no-decor">edward.hofmeister@focus.org</a><br>
+				<a href="tel:+16196558686" class="no-decor">(619) 655-8686</a>
+			</h4>
+		</div>
+		<div class="page-item text-left vert-center" style="padding: 0;">
+			<a href="https://www.focus.org" target="_blank" style="padding: 0 4%;">
+				<img class="contain no-lightbox" src="/2025/02/images/focus.png" alt="FOCUS logo">
+			</a>
+		</div>
+		<div class="page-item text-left vert-center" style="padding: 0;">
+			
+		</div>
+	</div>
+</div>
+
+<!-- News Insert -->
+<div class="page _50">
+	<div class="page-section _10">
+		<div class="page-item center-center">
+			<h2>You Are Invited To Join This Bible Study!</h2>
+		</div>
+	</div>
+	<div class="page-section _15">
+		<div class="page-item">
+			<p class="text-large">
+				Want to encounter God in scripture? Want to experience a FOCUS Bible study? Curtis Martin and Dr. Edward Sri are leading online Bible studies for everyone!
+			</p>
+		</div>
+	</div>
+	<div class="page-section _70">
+		<div class="page-item">
+			<a href="https://focusequip.org/messiah/" target="_blank"><img class="h100 no-lightbox" src="/2025/02/images/news.png " alt="" style="object-position: 50% 50%;"></a>
+		</div>
+	</div>
+ </div>
