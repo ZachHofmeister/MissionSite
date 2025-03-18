228e2e546ff39614c8858dc35d967b36fc3d19d2
ZachHofmeister <zachhof@me.com>
2025-03-18 17:24:01 -0600

    prepped dir for march newsletter

---
+++ 2025/03/pages.php
@@ -0,0 +1,189 @@
+<!-- Yours, O Lord, are the greatness, the power, the glory, the victory, and the majesty; for all that is in the heavens and on the earth is yours; yours is the kingdom, O Lord, and you are exalted as head above all. - 1 Chronicles 29:11 -->
+
+<!-- PAGE 1 -->
+<div class="page">
+	<!-- HEADER -->
+	<div class="page-section _10">
+		<div class="page-item vert-center">
+			<img class="contain no-lightbox" src="/2025/03/images/focus.png" alt="FOCUS logo">
+		</div>
+		<div class="page-item vert-center text-center w4 nopad">
+			<h1 class="cardo">The Mission at Mesa</h1>
+			<h5 class="italic" style="white-space: nowrap;">Zach Hofmeister<span class="no-style"> | </span>Colorado Mesa University<span class="no-style"> | </span>February 2025</h5>
+		</div>
+		<div class="page-item vert-center">
+			<img class="contain no-lightbox" src="/2025/03/images/cmu.png" alt="CMU logo">
+		</div>
+	</div>
+
+	<!-- MAIN IMAGE -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h100" src="/2025/03/images/awake-group.jpeg" alt="" style="object-position: 50% 50%;" data-caption="Our group of students on the awakening retreat!">
+		</div>
+	</div>
+
+	<!-- INTRO / PHOTO -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<div class="box">
+				<h2 class="italic">Dear Friends,</h2>
+				<p class="text-large">
+					Happy March! I hope lent is going well for you! I'm excited to share the highlights from mission this past month with you! Thank you so much for being a part of the mission here in your prayers and support for me!
+				</p>
+			</div>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2025/03/images/recap.jpeg" alt="" style="object-position: 50% 50%">
+			<p class="caption text-large">Students sharing their testimonies from SEEK at the start of the month</p>
+		</div>
+	</div>
+
+	<!-- Paragraph + Image -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<img class="h90" src="/2025/03/images/roses.jpeg" alt="" >
+			<p class="caption text-large">Handing out Valentine's Day roses</p>
+		</div>
+		<div class="page-item">
+			<h3>St. Valentine's Outreach</h3>
+			<p class="text-medium">
+				This year we've been focusing on calling our students, and ourselves, out of our comfort zone in campus outreach. For Valentine's Day, we handed out roses with Valentine's notes and invitations to Mesa Catholic. We had a lot more help from our students than normal, and had a lot of positive interactions with people through it!
+			</p>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE 2 -->
+<div class="page">
+	<!-- quote -->
+	<div class="page-section _10">
+		<div class="page-item center-center">
+			<p class="text-large text-center"><i>“And what you have heard from me before many witnesses entrust to faithful men who will be able to teach others also.”</i> - <a href="https://www.biblegateway.com/passage/?search=2%20Timothy%202&version=RSVCE" target="_blank">2 Timothy 2:2</a></p>
+		</div>
+	</div>
+
+	<!-- New Bible Studies -->
+	<div class="page-section _30">
+		<div class="page-item text-right">
+			<h3>New Bible Studies</h3>
+			<p class="text-medium">
+				Barrett started leading his first Bible study this month, and Gabe had his first experience co-leading my study. They have both been doing a good job leading and inviting their friends too. I've been preparing them to share Gospel invitations with study members soon - please pray for the Holy Spirit to encourage them!
+			</p>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2025/03/images/bstud-gabe.jpeg" alt="" style="object-position: 50% 50%;">
+			<p class="caption text-large">Gabe leading a Bible study session about confession</p>
+		</div>
+	</div>
+
+	<!-- photos -->
+	<div class="page-section _25">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2025/03/images/bstud-volleyball.jpeg" alt="" >
+			<p class="caption text-large">My discipleship men during their intramural volleyball game</p>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2025/03/images/awake-adoration.jpeg" alt="" >
+			<p class="caption text-large">The 24-hour Adoration chapel at Awakening to pray for retreaters</p>
+		</div>
+	</div>
+
+	<!-- Awakening Retreat -->
+	<div class="page-section _35">
+		<div class="page-item">
+			<img class="h80" src="/2025/03/images/awake-table.jpeg" alt="">
+			<p class="caption text-large">My small group on the Awakening retreat</p>
+		</div>
+		<div class="page-item">
+			<h3>Awakening Retreat</h3>
+			<p class="text-medium">
+				Towards the end of the month we had the Awakening retreat with our students. On the retreat we had Mass, Adoration, and confession, as well as lots of time listening to talks, and discussing how we can grow closer to the Lord. It was awesome to see students who went for the first time last year now giving talks and sharing their testimonies with new students!
+			</p>
+		</div>
+	</div>
+</div>
+
+<!-- PAGE 3 -->
+<div class="page">
+
+	<!-- photo-text -->
+	<div class="page-section _30">
+		<div class="page-item">
+			<h3 class="text-right">Other Highlights</h3>
+			<ul class="text-medium">
+				<li>Robert asking me to be his sponsor for his confirmation this Easter!</li>
+				<li>The Mesa Catholic superbowl party</li>
+				<li>Our first monthly leadership prayer meeting, bringing student leaders together to pray for the Holy Spirit's direction of campus mission</li>
+				<li>Seeing God work in amazing ways in our wedding planning, providing sponsors for our reception venue!</li>
+			</ul>
+		</div>
+		<div class="page-item">
+			<img class="h80" src="/2025/03/images/shooting.jpeg" alt="" style="object-position: 50% 50%;">
+			<p class="caption text-large">Skeet shooting with the Exodus 90 men's group</p>
+		</div>
+	</div>
+
+	<!-- Pics -->
+	<div class="page-section _20">
+		<div class="page-item">
+			<img class="h80" src="/2025/03/images/chicken.jpeg" alt="" style="object-position: 50% 50%;">
+			<p class="caption text-large">Getting Chick-fil-a with our students</p>
+		</div>
+		<div class="page-item">
+			<img class="photo-item h80" src="/2025/03/images/skiing.jpeg" alt="" style="object-position: 50% 50%;">
+			<p class="caption text-large">Cross-country skiing with Morgan (our regional director) and Haleigh</p>
+		</div>
+	</div>
+
+	<!-- PRAYER INTENTIONS -->
+	<div class="page-section _15">
+		<div class="page-item">
+			<h3>Prayer Requests</h3>
+			<p class="text-medium">
+				Please pray for our mission trip group going to Yucatán March 14-23 (Jacky, Joe, Drew, Nick, Kylee, Nadine, Hector, and others from Connecticut), for the health of our student Ace, and for my marriage prep. I'd love to know what you need prayers for too!
+			</p>
+		</div>
+	</div>
+
+	<!-- PHOTO / THANK YOU -->
+	<div class="page-section _25">
+		<div class="page-item">
+			<img class="photo-item h80" src="/2025/03/images/engaged.jpeg" alt="" style="object-position: 50% 17%;">
+			<p class="caption text-large">Our student Sophie took our engagement photos - she is amazing!</p>
+		</div>
+		<div class="page-item text-medium">
+			<div class="box">
+				<h2 class="italic">THANK YOU!</h2>
+				<p class="text-medium">Thank you for continuing to be on mission with me at CMU! I am praying that this season will bear fruit in your life to enter deeper into Christ's love for you!</p>
+				<p class="text-medium text-right">
+					In His love, Zach
+				</p>
+			</div>
+		</div>
+	</div>
+	
+	<!-- FOOTER -->
+	<div class="page-section _10" id="nl-footer"><script src="/js/nl-footer.js"></script></div>
+</div>
+
+<!-- News Insert -->
+<div class="page _50">
+	<div class="page-section _10">
+		<div class="page-item center-center">
+			<h2>FOCUS National News</h2>
+		</div>
+	</div>
+	<div class="page-section _15">
+		<div class="page-item">
+			<p class="text-large">
+				Here's a look at the impact that SEEK had across it's 3 locations this year! Check out all of the recorded talks at <a href="https://seekreplay.com" target="_blank">seekreplay.com</a>!
+			</p>
+		</div>
+	</div>
+	<div class="page-section _70">
+		<div class="page-item">
+			<a href="https://seekreplay.com" target="_blank"><img class="h100 no-lightbox" src="/2025/03/images/news.png " alt="" style="object-position: 50% 50%;"></a>
+		</div>
+	</div>
+ </div>
