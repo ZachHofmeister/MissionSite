c5b269741007c7c6afd7018033e922c12a3c3599
ZachHofmeister <17202322+ZachHofmeister@users.noreply.github.com>
2025-04-16 14:41:59 -0600

    finished March 2025 newsletter

--- 2025/03/pages.php
+++ 2025/03/pages.php
@@ -19,7 +19,7 @@
 	<!-- MAIN IMAGE -->
 	<div class="page-section _30">
 		<div class="page-item">
-			<img class="h100" src="/2025/03/images/ashwed.jpeg" alt="" style="object-position: 50% 50%;" data-caption="Our group of students on the awakening retreat!">
+			<img class="h100" src="/2025/03/images/sunday.jpeg" alt="" style="object-position: 50% 50%;" data-caption="Having a great evening with our students at our weekly Sunday dinner">
 		</div>
 	</div>
 
@@ -29,25 +29,25 @@
 			<div class="box">
 				<h2 class="italic">Dear Friends,</h2>
 				<p class="text-large">
-					I hope that you are doing well! This March was good, refreshing, and challenging all at the same time. Lent started and our campus had spring break and a mission trip! I'm excited to share all of the joys and struggles of mission with you.
+					I hope that you are doing well! This March was good, refreshing, and challenging all at the same time. Lent started and our campus had spring break and a mission trip! I'm excited to share the joys and struggles of mission with you.
 				</p>
 			</div>
 		</div>
 		<div class="page-item">
-			<img class="photo-item h80" src="/2025/03/images/recap.jpeg" alt="" style="object-position: 50% 50%">
-			<p class="caption text-large">caption</p>
+			<img class="photo-item h80" src="/2025/03/images/ashwed.jpeg" alt="" style="object-position: 50% 50%">
+			<p class="caption text-large">One of our on-campus Ash Wednesday Masses - lots of new students came!</p>
 		</div>
 	</div>
 
 	<!-- Paragraph + Image -->
 	<div class="page-section _30">
 		<div class="page-item">
-			<img class="h90" src="/2025/03/images/roses.jpeg" alt="" >
-			<p class="caption text-large">caption</p>
+			<img class="h80" src="/2025/03/images/yucatan.jpeg" alt="" >
+			<p class="caption text-large">The Mesa group on the mission trip to Yucatan</p>
 		</div>
 		<div class="page-item">
-			<h3>Section</h3>
-			<p class="text-medium">paragraph</p>
+			<h3>Mission Trip</h3>
+			<p class="text-medium">I was not part of the trip this year, but our students loved It! In Mexico, they got to teach catechism classes to kids, helped with a building project for a parish, and visited people in their homes to talk and pray with them. It was awesome to hear about it and see the impact it had, especially on Drew and Nick, who seem a little more outgoing!</p>
 		</div>
 	</div>
 </div>
@@ -55,45 +55,31 @@
 <!-- PAGE 2 -->
 <div class="page">
 	<!-- quote -->
-	<div class="page-section _10">
+	<div class="page-section _5">
 		<div class="page-item center-center">
-			<p class="text-large text-center"><i>“And what you have heard from me before many witnesses entrust to faithful men who will be able to teach others also.”</i> - <a href="https://www.biblegateway.com/passage/?search=2%20Timothy%202&version=RSVCE" target="_blank">2 Timothy 2:2</a></p>
+			<h3 class="cardo text-center"><i>“He delivered me, because He delighted in me”</i> - <a href="https://www.biblegateway.com/passage/?search=Psalm%2018&version=RSVCE" target="_blank">Psalm 18:19</a></h3>
 		</div>
 	</div>
 
-	<!-- New Bible Studies -->
-	<div class="page-section _30">
-		<div class="page-item text-right">
-			<h3>Section</h3>
-			<p class="text-medium">paragraph</p>
-		</div>
+	<!-- SECTION -->
+	<div class="page-section _60">
 		<div class="page-item">
-			<img class="photo-item h80" src="/2025/03/images/bstud-gabe.jpeg" alt="" style="object-position: 50% 50%;">
-			<p class="caption text-large">caption</p>
-		</div>
-	</div>
-
-	<!-- photos -->
-	<div class="page-section _25">
-		<div class="page-item">
-			<img class="photo-item h80" src="/2025/03/images/bstud-volleyball.jpeg" alt="" >
-			<p class="caption text-large">caption</p>
-		</div>
-		<div class="page-item">
-			<img class="photo-item h80" src="/2025/03/images/awake-adoration.jpeg" alt="" >
-			<p class="caption text-large">caption</p>
+			<h3>The Path of Mission Ahead</h3>
+			<p class="text-large">It's hard to believe, but we are about one month away from the end of the semester as I write this. This is my third year on mission with FOCUS, and it's been a difficult season of discerning where the Lord is calling me to go next year. Ultimately, I have decided I will be leaving staff with FOCUS this summer.</p>
+			<p class="text-large">I'm going to miss being on mission full-time, the close relationships with my students, and my team life that helps me to be rooted in prayer and joy. At the same time, I am excited to find a job where I can use and develop my talents with technology, have a normal schedule as I prepare to be married in September, and learn to be on mission in my workplace and parish.</p>
+			<p class="text-large">Preparing for this change has been really difficult for me, as I'm looking for what is to come and still trying to be fully engaged with the students the Lord has put in my life to share the Gospel with. At the same time, I am confident that it is the path the Lord is leading me on, and so it is good. Please pray for me to continue with confidence in Him, and to know how I can best witness to the Gospel in my time remaining.</p>
 		</div>
 	</div>
 
 	<!-- Awakening Retreat -->
 	<div class="page-section _35">
 		<div class="page-item">
-			<img class="h80" src="/2025/03/images/awake-table.jpeg" alt="">
-			<p class="caption text-large">caption</p>
+			<img class="h60" src="/2025/03/images/leaders.jpeg" alt="" style="object-position: 50% 15%;">
+			<p class="caption text-medium">Our Mesa Catholic leadership took an afternoon to pray and invite the Holy Spirit to lead us as we plan for the next school year. Some topics that arose were bringing the sacraments to more people, interfaith outreach, and growing the community!</p>
 		</div>
 		<div class="page-item">
-			<h3>Section</h3>
-			<p class="text-medium">paragraph</p>
+			<img class="h60" src="/2025/03/images/ocia.jpeg" alt="">
+			<p class="caption text-medium">I got brunch with some of the guys from the OCIA class one Sunday! At Easter, Robert, Elijah, and Brenden will be confirmed, and Brenden will also receive Jesus in the Eucharist for the first time! Praise God, and pray for them!</p>
 		</div>
 	</div>
 </div>
@@ -102,28 +88,30 @@
 <div class="page">
 
 	<!-- photo-text -->
-	<div class="page-section _30">
+	<div class="page-section _25">
 		<div class="page-item">
 			<h3 class="text-right">Other Highlights</h3>
 			<ul class="text-medium">
-				<li>Robert asking me to be his sponsor for his confirmation this Easter!</li>
+				<li>An awesome and open conversation with Nathan, a student who came up to our table on Ash Wednesday with questions about the Catholic faith</li>
+				<li>Going to Mass and confession with Robert and Elijah over spring break</li>
+				<li>Taking a weekend trip to Denver and Colorado Springs with the team</li>
 			</ul>
 		</div>
 		<div class="page-item">
-			<img class="h80" src="/2025/03/images/shooting.jpeg" alt="" style="object-position: 50% 50%;">
-			<p class="caption text-large">caption</p>
+			<img class="h80" src="/2025/03/images/team.jpeg" alt="" style="object-position: 50% 50%;">
+			<p class="caption text-large">Our CMU team from last year reunited in Denver!</p>
 		</div>
 	</div>
 
 	<!-- Pics -->
-	<div class="page-section _20">
+	<div class="page-section _25">
 		<div class="page-item">
-			<img class="h80" src="/2025/03/images/chicken.jpeg" alt="" style="object-position: 50% 50%;">
-			<p class="caption text-large">caption</p>
+			<img class="photo-item h80" src="/2025/03/images/springs.jpeg" alt="" style="object-position: 50% 50%;">
+			<p class="caption text-large">We got together with our FOCUS friends at Denver University</p>
 		</div>
 		<div class="page-item">
-			<img class="photo-item h80" src="/2025/03/images/skiing.jpeg" alt="" style="object-position: 50% 50%;">
-			<p class="caption text-large">caption</p>
+			<img class="photo-item h80" src="/2025/03/images/cadets.jpeg" alt="" style="object-position: 50% 50%;">
+			<p class="caption text-large">I got to catch up with Pete and Joe from USAFA! Pray for them as they graduate this semester! ✈️</p>
 		</div>
 	</div>
 
@@ -132,7 +120,7 @@
 		<div class="page-item">
 			<h3>Prayer Requests</h3>
 			<p class="text-medium">
-				Please pray for our mission trip group going to Yucatán March 14-23 (Jacky, Joe, Drew, Nick, Kylee, Nadine, Hector, and others from Connecticut), for the health of our student Ace, and for my marriage prep. I'd love to know what you need prayers for too!
+				Please pray for Ace's health, for Brenden, Elijah, Robert, and Miranda as they receive the sacraments at Easter, for the men I am discipling, and for my job search and marriage prep! Thank you so much! Let me know how I can pray for you!
 			</p>
 		</div>
 	</div>
@@ -140,13 +128,13 @@
 	<!-- PHOTO / THANK YOU -->
 	<div class="page-section _25">
 		<div class="page-item">
-			<img class="photo-item h80" src="/2025/03/images/engaged.jpeg" alt="" style="object-position: 50% 17%;">
-			<p class="caption text-large">caption</p>
+			<img class="photo-item h80" src="/2025/03/images/allison.jpeg" alt="" style="object-position: 50% 17%;">
+			<p class="caption text-large">Have a blessed Holy Week and Easter!</p>
 		</div>
 		<div class="page-item text-medium">
 			<div class="box">
 				<h2 class="italic">THANK YOU!</h2>
-				<p class="text-medium">Thank you for continuing to be on mission with me at CMU! I am praying that this season will bear fruit in your life to enter deeper into Christ's love for you!</p>
+				<p class="text-medium">Thank you for being on mission with me as I strive to finish this semester well! I look forward to reaching out to you and sharing more in the Easter season!</p>
 				<p class="text-medium text-right">
 					In His love, Zach
 				</p>
@@ -157,24 +145,3 @@
 	<!-- FOOTER -->
 	<div class="page-section _10" id="nl-footer"><script src="/js/nl-footer.js"></script></div>
 </div>
-
-<!-- News Insert -->
-<div class="page _50">
-	<div class="page-section _10">
-		<div class="page-item center-center">
-			<h2>FOCUS National News</h2>
-		</div>
-	</div>
-	<div class="page-section _15">
-		<div class="page-item">
-			<p class="text-large">
-				Here's a look at the impact that SEEK had across it's 3 locations this year! Check out all of the recorded talks at <a href="https://seekreplay.com" target="_blank">seekreplay.com</a>!
-			</p>
-		</div>
-	</div>
-	<div class="page-section _70">
-		<div class="page-item">
-			<a href="https://seekreplay.com" target="_blank"><img class="h100 no-lightbox" src="/2025/03/images/news.png " alt="" style="object-position: 50% 50%;"></a>
-		</div>
-	</div>
- </div>
